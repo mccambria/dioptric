c93beeefdc74fa6d5b8273013b2000835f2b1cc6
mccambria <matthew.carl.cambria@gmail.com>
2019-04-10 22:02:51 -0500

    set up node on client init

--- labrad_manager.bat
+++ labrad_manager.bat
@@ -1,3 +1,4 @@
 start cmd /k C:\Program_Files\Labrad\scalabrad-0.8.3\bin\labrad.bat
 start cmd /k C:\Program_Files\Labrad\scalabrad-web-server-2.0.5\bin\labrad-web.bat
 start chrome /new-window http://localhost:7667
+call python -m labrad.node
