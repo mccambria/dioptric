e6037af8c1197b3e76871bf206da0e84b0565485
GardillA <gardill@wisc.edu>
2019-06-04 15:06:17 -0500

    Update relaxation_rates.py

--- analysis/relaxation_rates.py
+++ analysis/relaxation_rates.py
@@ -16,9 +16,32 @@ import json
 from scipy import asarray as ar,exp
 from scipy.optimize import curve_fit
 
-def relaxation_rate_analysis(folder_name)
+def relaxation_rate_analysis(folder_name):
     
-    os.system("ls *.txt")
+    directory = 'G:/Team Drives/Kolkowitz Lab Group/nvdata/t1_double_quantum/' 
+    
+    file_list = []
+    for file in os.listdir('{}/{}'.format(directory, folder_name)):
+        if file.endswith(".txt"):
+            file_list.append(file)
+
+#    print(file_list)
+    
+    for file in file_list:
+        with open('{}/{}/{}'.format(directory, folder_name, file)) as json_file:
+            data = json.load(json_file)
+            init_state = data['init_state']
+            read_state = data['read_state']
+            
+            relaxation_time_range = data['relaxation_time_range']
+            num_steps = data['num_steps']
+            num_runs = data['num_runs']
+            
+            if 
+            print('({}, {})'.format(init_state, read_state))
+            
+            # numpy.concatenate((a, b), axis=1)
+        
     # import arrays to work with
     # import data, reading in the readout out states to sort them
     # subreact the relative data to get the two functions
@@ -26,4 +49,6 @@ def relaxation_rate_analysis(folder_name)
     # split up the num_runs into various amounts
     # fit each bin
     # average and st dev
+if __name__ == '__main__':
     
+    relaxation_rate_analysis('2019-04-30-NV2_29MHzSplitting_important_data')
