b542d1562f61fe2af7d8f7522c2a9778d2341e8d
GardillA <gardill@wisc.edu>
2019-06-07 16:15:14 -0500

    Update relaxation_rates.py

--- analysis/relaxation_rates.py
+++ analysis/relaxation_rates.py
@@ -11,6 +11,8 @@ deviation.
 This file only works if all the experiments in a folder have the same number
 of num_runs
 
+
+
 @author: Aedan
 """
 import os
@@ -19,6 +21,8 @@ import json
 from scipy import asarray as ar,exp
 from scipy.optimize import curve_fit
 import matplotlib.pyplot as plt
+
+import utils.tool_belt as tool_belt
 
 #%%
 
@@ -32,7 +36,8 @@ def plus_relaxation_eq(t, gamma, omega, amp, offset):
 
 # %%
 
-def relaxation_rate_analysis(folder_name):
+def relaxation_rate_analysis(folder_name, bin_size, doPlot = False,
+                             save_data = True):
     
     directory = 'G:/Shared drives/Kolkowitz Lab Group/nvdata/t1_double_quantum/' 
     
@@ -41,6 +46,8 @@ def relaxation_rate_analysis(folder_name):
     for file in os.listdir('{}/{}'.format(directory, folder_name)):
         if file.endswith(".txt"):
             file_list.append(file)
+            
+    print(file_list)
       
     # Get the number of runs to create the empty arrays from the first file in 
     # the list. This requires all the relaxation measurements to have the same
@@ -69,7 +76,7 @@ def relaxation_rate_analysis(folder_name):
     omega_rate_list = []
     omega_amp_list = []
     omega_offset_list = []
-    gamma_list = []
+    gamma_rate_list = []
     gamma_amp_list = []
     gamma_offset_list = []
     
@@ -95,6 +102,9 @@ def relaxation_rate_analysis(folder_name):
             time_array = numpy.linspace(min_relaxation_time, max_relaxation_time,
                           num=num_steps) 
             
+            uwave_freq_init = data['uwave_freq_init']
+            uwave_freq_read = data['uwave_freq_read']
+            
             # Check to see which data set the file is for, and append the data
             # to the corresponding array
             if init_state == 0 and read_state == 0:
@@ -112,7 +122,6 @@ def relaxation_rate_analysis(folder_name):
                                                     sig_counts, axis = 1)
                 zero_plus_ref_counts = numpy.append(zero_plus_ref_counts, 
                                                     ref_counts, axis = 1)
-                zero_plus_time = numpy.append(zero_plus_time, time_array)
 
             if init_state == 1 and read_state == 1:
                 plus_plus_sig_counts = numpy.append(plus_plus_sig_counts, 
@@ -129,7 +138,10 @@ def relaxation_rate_analysis(folder_name):
                                                     sig_counts, axis = 1)
                 plus_minus_ref_counts = numpy.append(plus_minus_ref_counts, 
                                                     ref_counts, axis = 1)
-                plus_minus_time = numpy.append(plus_minus_time, time_array)
+                
+                splitting_MHz = abs(uwave_freq_init - uwave_freq_read)
+                
+                
     
     # Delete the NaNs from all the arrays. There might be a better way to fill
     # the arrays, but this should work for now
@@ -139,7 +151,6 @@ def relaxation_rate_analysis(folder_name):
     
     zero_plus_sig_counts = numpy.delete(zero_plus_sig_counts, 0, axis = 1)
     zero_plus_ref_counts = numpy.delete(zero_plus_ref_counts, 0, axis = 1)
-    zero_plus_time = numpy.delete(zero_plus_time, 0)
     
     plus_plus_sig_counts = numpy.delete(plus_plus_sig_counts, 0, axis = 1)
     plus_plus_ref_counts = numpy.delete(plus_plus_ref_counts, 0, axis = 1)
@@ -147,77 +158,154 @@ def relaxation_rate_analysis(folder_name):
     
     plus_minus_sig_counts = numpy.delete(plus_minus_sig_counts, 0, axis = 1)
     plus_minus_ref_counts = numpy.delete(plus_minus_ref_counts, 0, axis = 1)
-    plus_minus_time = numpy.delete(plus_minus_time, 0)
 
-# %% Fit to the (0,0) - (0,1) data to find Omega
+# %% Fit the data based on the bin size
+    
+    i = 0
+    
+    while i < (num_runs - 1):
+        #Fit to the (0,0) - (0,1) data to find Omega
+     
+        zero_zero_avg_sig_counts = numpy.average(zero_zero_sig_counts[i:i+bin_size, ::], axis=0)
+        zero_zero_avg_ref_counts = numpy.average(zero_zero_ref_counts[i:i+bin_size, ::], axis=0)
+        
+        zero_zero_norm_avg_sig = zero_zero_avg_sig_counts / zero_zero_avg_ref_counts
+               
+        zero_plus_avg_sig_counts = numpy.average(zero_plus_sig_counts[i:i+bin_size, ::], axis=0)
+        zero_plus_avg_ref_counts = numpy.average(zero_plus_ref_counts[i:i+bin_size, ::], axis=0)
+        
+        zero_plus_norm_avg_sig = zero_plus_avg_sig_counts / zero_plus_avg_ref_counts 
+    
+        # Define the counts for the zero relaxation equation
+        zero_relaxation_counts =  zero_zero_norm_avg_sig - zero_plus_norm_avg_sig
+        
+        init_params = (1.0, 0.4, 0)
+        opti_params, cov_arr = curve_fit(zero_relaxation_eq, zero_zero_time,
+                                            zero_relaxation_counts, p0 = init_params)
 
-    # let's first just try finding the relaxation rate for all the data together.
-    
-    zero_zero_avg_sig_counts = numpy.average(zero_zero_sig_counts, axis=0)
-    zero_zero_avg_ref_counts = numpy.average(zero_zero_ref_counts, axis=0)
-    
-    zero_zero_norm_avg_sig = zero_zero_avg_sig_counts / zero_zero_avg_ref_counts
-           
-    zero_plus_avg_sig_counts = numpy.average(zero_plus_sig_counts, axis=0)
-    zero_plus_avg_ref_counts = numpy.average(zero_plus_ref_counts, axis=0)
-    
-    zero_plus_norm_avg_sig = zero_plus_avg_sig_counts / zero_plus_avg_ref_counts 
-    
-    # Define the counts for the zero relaxation equation
-    zero_relaxation_counts =  zero_zero_norm_avg_sig - zero_plus_norm_avg_sig
-    
-    init_params = (1.2, 0.36, 0)
-    opti_params, cov_arr = curve_fit(zero_relaxation_eq, zero_zero_time,
-                                        zero_relaxation_counts, p0 = init_params)
+        omega_rate_list.append(opti_params[0])
+        omega_amp_list.append(opti_params[1])
+        omega_offset_list.append(opti_params[2])
+        omega = opti_params[0]
+        
+        # Plotting the data
+        if doPlot:
+            fig, axes_pack = plt.subplots(1, 2, figsize=(17, 8))
+            ax = axes_pack[0]
+            ax.plot(zero_zero_time, zero_relaxation_counts, 'bo', label = 'data')
+            ax.plot(zero_zero_time_linspace, 
+                    zero_relaxation_eq(zero_zero_time_linspace, *opti_params), 
+                    'r', label = 'fit') 
+            ax.set_xlabel('Relaxation time (ms)')
+            ax.set_ylabel('Normalized signal Counts')
+            ax.set_title('(0,0) - (0,+1)')
+            ax.legend()
+            text = r'$\Omega = $ {} kHz'.format('%.2f'%opti_params[0])
 
-    omega_rate_list.append(opti_params[0])
-    omega_amp_list.append(opti_params[1])
-    omega_offset_list.append(opti_params[2])
-    omega = opti_params[0]
+            props = dict(boxstyle="round", facecolor="wheat", alpha=0.5)
+            ax.text(0.55, 0.95, text, transform=ax.transAxes, fontsize=12,
+                    verticalalignment='top', bbox=props)
     
-    fig, axes_pack = plt.subplots(1, 2, figsize=(17, 8))
-    ax = axes_pack[0]
-    ax.plot(zero_zero_time, zero_relaxation_counts, 'bo', label = 'data')
-    ax.plot(zero_zero_time_linspace, 
-            zero_relaxation_eq(zero_zero_time_linspace, *opti_params), 
-            'r', label = 'fit')  
-    
-# %% Fit to the (1,1) - (1,-1) data to find Gamma
-    
-    plus_plus_avg_sig_counts = numpy.average(plus_plus_sig_counts, axis=0)
-    plus_plus_avg_ref_counts = numpy.average(plus_plus_ref_counts, axis=0)
-    
-    plus_plus_norm_avg_sig = plus_plus_avg_sig_counts / plus_plus_avg_ref_counts
-           
-    plus_minus_avg_sig_counts = numpy.average(plus_minus_sig_counts, axis=0)
-    plus_minus_avg_ref_counts = numpy.average(plus_minus_ref_counts, axis=0)
-    
-    plus_minus_norm_avg_sig = plus_minus_avg_sig_counts / plus_minus_avg_ref_counts
-    
-    # Define the counts for the plus relaxation equation
-    plus_relaxation_counts =  plus_plus_norm_avg_sig - plus_minus_norm_avg_sig
-    
-    
-    init_params = (100, 0.50, 0)
-    # create a temporary fitting equation that passes in the omega value just found
-    plus_relaxation_tmp = lambda t, gamma, amp, offset: plus_relaxation_eq(t, gamma, omega, amp, offset)
-    opti_params, cov_arr = curve_fit(plus_relaxation_tmp, 
-                                     plus_plus_time, plus_relaxation_counts, p0 = init_params)
-    
-    gamma_rate_list.append(opti_params[0])
-    gamma_amp_list.append(opti_params[2])
-    gamma_offset_list.append(opti_params[3])
-    
-    ax = axes_pack[1]
-    ax.plot(plus_plus_time, plus_relaxation_counts, 'bo')
-    ax.plot(plus_plus_time_linspace, 
-            plus_relaxation_tmp(plus_plus_time_linspace, *opti_params), 
-            'r', label = 'fit')   
+    # %% Fit to the (1,1) - (1,-1) data to find Gamma
+        
+        plus_plus_avg_sig_counts = numpy.average(plus_plus_sig_counts[i:i+bin_size, ::], axis=0)
+        plus_plus_avg_ref_counts = numpy.average(plus_plus_ref_counts[i:i+bin_size, ::], axis=0)
+        
+        plus_plus_norm_avg_sig = plus_plus_avg_sig_counts / plus_plus_avg_ref_counts
+               
+        plus_minus_avg_sig_counts = numpy.average(plus_minus_sig_counts[i:i+bin_size, ::], axis=0)
+        plus_minus_avg_ref_counts = numpy.average(plus_minus_ref_counts[i:i+bin_size, ::], axis=0)
+        
+        plus_minus_norm_avg_sig = plus_minus_avg_sig_counts / plus_minus_avg_ref_counts
+        
+        # Define the counts for the plus relaxation equation
+        plus_relaxation_counts =  plus_plus_norm_avg_sig - plus_minus_norm_avg_sig
+        
+        
+        init_params = (100, 0.40, 0)
+        
+        # create a temporary fitting equation that passes in the omega value just found
+        plus_relaxation_tmp = lambda t, gamma, amp, offset: plus_relaxation_eq(t, gamma, omega, amp, offset)
+        opti_params, cov_arr = curve_fit(plus_relaxation_tmp, 
+                                         plus_plus_time, plus_relaxation_counts, p0 = init_params)
+        
+        gamma_rate_list.append(opti_params[0])
+        gamma_amp_list.append(opti_params[1])
+        gamma_offset_list.append(opti_params[2])
+        
+        # Advance the index
+        i = i + bin_size
+        
+        # Plotting
+        if doPlot:
+            ax = axes_pack[1]
+            ax.plot(plus_plus_time, plus_relaxation_counts, 'bo')
+            ax.plot(plus_plus_time_linspace, 
+                    plus_relaxation_tmp(plus_plus_time_linspace, *opti_params), 
+                    'r', label = 'fit')   
+            ax.set_xlabel('Relaxation time (ms)')
+            ax.set_ylabel('Normalized signal Counts')
+            ax.set_title('(+1,+1) - (+1,-1)')
+            ax.legend()
+            text = r'$\gamma = $ {} kHz'.format('%.2f'%opti_params[0])
 
-    fig.canvas.draw()
-
+            props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+            ax.text(0.55, 0.95, text, transform=ax.transAxes, fontsize=12,
+                    verticalalignment='top', bbox=props)
+            
+            fig.canvas.draw()
+            fig.canvas.flush_events()
+    
+    omega_average = numpy.average(omega_rate_list)
+    omega_stdev = numpy.std(omega_rate_list)
+    
+    gamma_average = numpy.average(gamma_rate_list)
+    gamma_stdev = numpy.std(gamma_rate_list)
+    
+    print('Omega list: {} \nGamma list: {}'.format(omega_rate_list, gamma_rate_list))
+    
+# %% Saving data
+    if save_data: 
+        time_stamp = tool_belt.get_time_stamp()
+        raw_data = {'time_stamp': time_stamp,
+                    'level_splitting': splitting_MHz,
+                    'level_splitting-units': 'MHz',
+                    'num_runs': num_runs,
+                    'bin_size': bin_size,
+                    'omega_average': omega_average,
+                    'omega_average-units': 'kHz',
+                    'omega_stdev': omega_stdev,
+                    'omega_stdev-units': 'kHz',
+                    'gamma_average': gamma_average,
+                    'gamma_average-units': 'kHz',
+                    'gamma_stdev': gamma_stdev,
+                    'gamma_stdev-units': 'kHz',
+                    'omega_rate_list': omega_rate_list,
+                    'omega_rate_list-units': 'kHz',
+                    'omega_amp_list': omega_rate_list,
+                    'omega_amp_list-units': 'arb',
+                    'omega_offset_list': omega_offset_list,
+                    'omega_offset_list-units': 'arb',
+                    'gamma_rate_list': gamma_rate_list,
+                    'gamma_rate_list-units': 'kHz',
+                    'gamma_amp_list': gamma_rate_list,
+                    'gamma_amp_list-units': 'arb',
+                    'gamma_offset_list': gamma_offset_list,
+                    'gamma_offset_list-units': 'arb'}
+        
+        file_name = time_stamp + '_' + str('%.1f'%splitting_MHz) + '_MHz_splitting'
+        file_path = '{}/{}/{}'.format(directory, folder_name, file_name)
+        print(file_path)
+        # tool_belt.save_raw_data(raw_data, file_path)
+        
+        with open(file_path + '.txt', 'w') as file:
+            json.dump(raw_data, file, indent=2)
+      
+                
+    
 # %%
     
 if __name__ == '__main__':
     
-    relaxation_rate_analysis('2019-05-10-NV1_32MHzSplitting_important_data')
+    relaxation_rate_analysis('2019-05-10-NV1_32MHzSplitting_important_data', 20,
+                            False)
