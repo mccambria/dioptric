8ea346ce8cdc64d5d761a0387ebd40f6610ce1f0
mccambria <matthew.carl.cambria@gmail.com>
2019-06-16 11:21:24 -0500

    Update archive_branches.py

--- utils/archive_branches.py
+++ utils/archive_branches.py
@@ -24,10 +24,10 @@ from git import Repo
 # %% Input parameters
 
 # Repo path
-repo_path = 'C:\\Users\\kolkowitz\\Documents\\' \
-     'GitHub\\kolkowitz-nv-experiment-v1.0'
-#repo_path = 'C:\\Users\\Matt\\' \
-#    'GitHub\\kolkowitz-nv-experiment-v1.0'
+# repo_path = 'C:\\Users\\kolkowitz\\Documents\\' \
+#      'GitHub\\kolkowitz-nv-experiment-v1.0'
+repo_path = 'C:\\Users\\Matt\\' \
+    'GitHub\\kolkowitz-nv-experiment-v1.0'
 
 # List of branch names
 branches_to_archive = ['debug-function']
