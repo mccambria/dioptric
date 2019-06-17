603ee0ac1dfca98fe5449ff94710475075c5285e
mccambria <matthew.carl.cambria@gmail.com>
2019-06-16 22:16:45 -0500

    attempt naive padding and template matching

--- minorroutines/drift_from_reference_image.py
+++ minorroutines/drift_from_reference_image.py
@@ -13,6 +13,9 @@ Created on Sun Jun 16 11:38:17 2019
 
 import majorroutines.image_sample as image_sample
 import utils.tool_belt as tool_belt
+import cv2
+import utils.image_processing as image_processing
+from matplotlib import pyplot as plt
 
 
 # %% Main
@@ -24,19 +27,45 @@ def main(cxn, ref_file_name, nd_filter, apd_indices):
     """
 
     # %% Get the reference image
+
     ref_data = tool_belt.get_raw_data('image_sample', ref_file_name)
 
     # %% Get a new image
 
     # Take a new image using the same parameters
-    new_data = image_sample.main(cxn, ref_data['coords'], nd_filter,
-                          ref_data['x_range'], ref_data['y_range'],
-                          ref_data['num_steps'], apd_indices,
-                          save_data=False, plot_data=False)
+    # new_data = image_sample.main(cxn, ref_data['coords'], nd_filter,
+    #                       ref_data['x_range'], ref_data['y_range'],
+    #                       ref_data['num_steps'], apd_indices,
+    #                       save_data=False, plot_data=False)
+
+    # Test
+    new_data = tool_belt.get_raw_data('image_sample',
+                                      '2019-06-14_16-36-48_ayrton12')
 
     # %% Calculate the shift
 
+    # Get the image arrays
+    ref_img_array = ref_data['img_array']
+    new_img_array = new_data['img_array']
 
+    # convert to 8 bit
+    ref_img_array = image_processing.convert_to_8bit(ref_img_array)
+    new_img_array = image_processing.convert_to_8bit(new_img_array)
+
+    # Add a border to the reference image so that we can use template matching
+    img_shape = ref_img_array.shape
+    ver_border_size = img_shape[0] // 2
+    hor_border_size = img_shape[1] // 2
+    ref_img_array = cv2.copyMakeBorder(ref_img_array,
+                       top=ver_border_size, bottom=ver_border_size,
+                       left=hor_border_size, right=hor_border_size,
+                       borderType=cv2.BORDER_CONSTANT, value=0)
+
+    res = cv2.matchTemplate(ref_img_array, new_img_array,
+                            cv2.TM_CCOEFF_NORMED)
+
+    fig, ax = plt.subplots(figsize=(5,5))
+    ax.imshow(res)
 
 
 # %% Run the file
@@ -47,4 +76,4 @@ def main(cxn, ref_file_name, nd_filter, apd_indices):
 # the script that you set up here.
 if __name__ == '__main__':
 
-    pass
+    main(None, '2019-06-14_16-28-13_ayrton12', None, None)
