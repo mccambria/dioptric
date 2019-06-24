c00cd401e7ee578fbed789d1a9a9518ac768ee4f
GardillA <gardill@wisc.edu>
2019-06-24 13:44:00 -0500

    Update spectrometer_comparison.py

--- analysis/spectrometer_comparison.py
+++ analysis/spectrometer_comparison.py
@@ -21,28 +21,28 @@ counts_linspace = numpy.linspace(10, 1000000, 1000)
 
 # %%
 def SN_t(t, *params):
-    quantum_eff, read_noise, dark_current, columns, rows, counts_p_sec, int_time = params
+    quantum_eff, read_noise, dark_current, px_p_columns, px_p_rows, counts_p_sec, int_time = params
     
-    photon_flux_p_pixel = counts_p_sec / (columns * rows)
+    photon_flux_p_pixel = counts_p_sec / (px_p_columns * px_p_rows)
     
-    signal = t * quantum_eff * photon_flux_p_pixel
+    signal = t * quantum_eff * photon_flux_p_pixel * px_p_columns
     
-    N_R = read_noise**2
-    N_D = dark_current * t 
-    N_S = photon_flux_p_pixel * quantum_eff * t
+    N_R = (read_noise * px_p_columns)**2
+    N_D = dark_current * t * px_p_columns
+    N_S = photon_flux_p_pixel * quantum_eff * t * px_p_columns
     
     return signal / numpy.sqrt(N_R + N_D + N_S)
 
 def SN_f(c, *params):
-    quantum_eff, read_noise, dark_current, columns, rows, counts_p_sec, int_time = params
+    quantum_eff, read_noise, dark_current, px_p_columns, px_p_rows, counts_p_sec, int_time = params
         
-    photon_flux_p_pixel = c / (columns * rows)
+    photon_flux_p_pixel = c / (px_p_columns * px_p_rows)
 
-    signal = int_time * quantum_eff * photon_flux_p_pixel
+    signal = int_time * quantum_eff * photon_flux_p_pixel * px_p_columns
     
-    N_R = read_noise**2
-    N_D = dark_current * int_time 
-    N_S = photon_flux_p_pixel * quantum_eff * int_time
+    N_R = (read_noise * px_p_columns)**2
+    N_D = dark_current * int_time * px_p_columns
+    N_S = photon_flux_p_pixel * quantum_eff * int_time * px_p_columns
     
     return signal / numpy.sqrt(N_R + N_D + N_S)
 
@@ -65,15 +65,15 @@ pi_OE = (0.5, 2.5, 0.0008, 100, 1340, counts_p_sec, int_time)
 time_fig, ax = plt.subplots(1, 1, figsize=(10, 8))
 
 # Integration time
-ax.plot(int_time_linspace, SN_t(int_time_linspace, *horiba_BIDD), label = 'Horiba BIDD')
-#ax.plot(int_time_linspace, SN_t(int_time_linspace, *horiba_BI), label = 'Horiba BI')
-#ax.plot(int_time_linspace, SN_t(int_time_linspace, *horiba_OE), label = 'Horiba OE')
+ax.plot(int_time_linspace, SN_t(int_time_linspace, *horiba_BIDD), 'r--', label = 'Horiba BIDD')
+ax.plot(int_time_linspace, SN_t(int_time_linspace, *horiba_BI), label = 'Horiba BI')
+ax.plot(int_time_linspace, SN_t(int_time_linspace, *horiba_OE), label = 'Horiba OE')
 
-ax.plot(int_time_linspace, SN_t(int_time_linspace, *andor_BIDD), 'r--', label = 'Andor BIDD')
+#ax.plot(int_time_linspace, SN_t(int_time_linspace, *andor_BIDD), 'r--', label = 'Andor BIDD')
 #ax.plot(int_time_linspace, SN_t(int_time_linspace, *andor_BI), label = 'Andor BI')
 #ax.plot(int_time_linspace, SN_t(int_time_linspace, *andor_OE), label = 'Andor OE')
 
-ax.plot(int_time_linspace, SN_t(int_time_linspace, *pi_BIDD), label = 'PI BIDD')
+#ax.plot(int_time_linspace, SN_t(int_time_linspace, *pi_BIDD), 'r--', label = 'PI BIDD')
 #ax.plot(int_time_linspace, SN_t(int_time_linspace, *pi_BI), label = 'PI BI')
 #ax.plot(int_time_linspace, SN_t(int_time_linspace, *pi_OE), label = 'PI FI')
 
@@ -88,15 +88,15 @@ time_fig.canvas.flush_events()
 
 flux_fig, ax = plt.subplots(1, 1, figsize=(10, 8))
 
-ax.plot(counts_linspace, SN_f(counts_linspace, *horiba_BIDD), label = 'Horiba BIDD')
-#ax.plot(counts_linspace, SN_f(counts_linspace, *horiba_BI), label = 'Horiba BI')
-#ax.plot(counts_linspace, SN_f(counts_linspace, *horiba_OE), label = 'Horiba OE')
+ax.plot(counts_linspace, SN_f(counts_linspace, *horiba_BIDD), 'r--', label = 'Horiba BIDD')
+ax.plot(counts_linspace, SN_f(counts_linspace, *horiba_BI), label = 'Horiba BI')
+ax.plot(counts_linspace, SN_f(counts_linspace, *horiba_OE), label = 'Horiba OE')
 
-ax.plot(counts_linspace, SN_f(counts_linspace, *andor_BIDD), 'r--', label = 'Andor BIDD')
+#ax.plot(counts_linspace, SN_f(counts_linspace, *andor_BIDD), 'r--', label = 'Andor BIDD')
 #ax.plot(counts_linspace, SN_f(counts_linspace, *andor_BI), label = 'Andor BI')
 #ax.plot(counts_linspace, SN_f(counts_linspace, *andor_OE), label = 'Andor OE')
 
-ax.plot(counts_linspace, SN_f(counts_linspace, *pi_BIDD), label = 'PI BIDD')
+#ax.plot(counts_linspace, SN_f(counts_linspace, *pi_BIDD), 'r--', label = 'PI BIDD')
 #ax.plot(counts_linspace, SN_f(counts_linspace, *pi_BI), label = 'PI BI')
 #ax.plot(counts_linspace, SN_f(counts_linspace, *pi_OE), label = 'PI FI')
 
