d2d83756989ed4dcec467991f02fe161bd0d2b5a
GardillA <gardill@wisc.edu>
2019-06-25 11:11:16 -0500

    Update spectrometer_comparison.py

--- analysis/spectrometer_comparison.py
+++ analysis/spectrometer_comparison.py
@@ -15,64 +15,64 @@ counts_p_sec = 10**5
 
 int_time = 100
 
-int_time_linspace = numpy.linspace(0.01,10**7, 1000)
+int_time_linspace = numpy.linspace(0.01,10**2, 1000)
 
-counts_linspace = numpy.linspace(10, 10**10, 1000)
+counts_linspace = numpy.linspace(10, 10**6, 1000)
 
 # %%
 def SN_t(t, *params):
     quantum_eff, read_noise, dark_current, px_p_columns, px_p_rows, counts_p_sec, int_time = params
     
-    photon_flux_p_pixel = counts_p_sec / (px_p_columns * px_p_rows)
+    photon_flux_p_column = counts_p_sec / (px_p_columns)
     
-    signal = t * quantum_eff * photon_flux_p_pixel * px_p_columns
+    signal = photon_flux_p_column * quantum_eff * t
     
-    N_R = (read_noise * px_p_columns)**2
+    N_R = (read_noise)**2
     N_D = dark_current * t * px_p_columns
-    N_S = photon_flux_p_pixel * quantum_eff * px_p_columns * t
+    N_S = photon_flux_p_column * quantum_eff * t
     
     return signal / numpy.sqrt(N_R + N_D + N_S)
 
 def SN_f(c, *params):
     quantum_eff, read_noise, dark_current, px_p_columns, px_p_rows, counts_p_sec, int_time = params
         
-    photon_flux_p_pixel = c / (px_p_columns * px_p_rows)
+    photon_flux_p_column = c / (px_p_columns)
 
-    signal = int_time * quantum_eff * photon_flux_p_pixel
+    signal = int_time * quantum_eff * photon_flux_p_column
     
     N_R = (read_noise)**2
-    N_D = dark_current * int_time 
-    N_S = photon_flux_p_pixel * quantum_eff * int_time 
+    N_D = dark_current * int_time * px_p_columns
+    N_S = photon_flux_p_column * quantum_eff * int_time 
     
     return signal / numpy.sqrt(N_R + N_D + N_S)
 
-def shot_noise_SNR_counts(c, *params):
-    quantum_eff, read_noise, dark_current, px_p_columns, px_p_rows, counts_p_sec, int_time = params
-    
-    photon_flux_p_pixel = c / (px_p_columns * px_p_rows)
-    
-    return numpy.sqrt(photon_flux_p_pixel * quantum_eff * int_time)
-
-def read_noise_SNR_counts(c, *params):
-    quantum_eff, read_noise, dark_current, px_p_columns, px_p_rows, counts_p_sec, int_time = params
-    
-    photon_flux_p_pixel = c / (px_p_columns * px_p_rows)
-    
-    return photon_flux_p_pixel * px_p_columns * quantum_eff * int_time / read_noise
-
-def shot_noise_SNR_time(t, *params):
-    quantum_eff, read_noise, dark_current, px_p_columns, px_p_rows, counts_p_sec, int_time = params
-    
-    photon_flux_p_pixel = counts_p_sec / (px_p_columns * px_p_rows)
-    
-    return numpy.sqrt(photon_flux_p_pixel * quantum_eff * t )
-
-def read_noise_SNR_time(t, *params):
-    quantum_eff, read_noise, dark_current, px_p_columns, px_p_rows, counts_p_sec, int_time = params
-    
-    photon_flux_p_pixel = counts_p_sec / (px_p_columns * px_p_rows)
-    
-    return photon_flux_p_pixel * px_p_columns * quantum_eff * t / read_noise
+#def shot_noise_SNR_counts(c, *params):
+#    quantum_eff, read_noise, dark_current, px_p_columns, px_p_rows, counts_p_sec, int_time = params
+#    
+#    photon_flux_p_pixel = c / (px_p_columns * px_p_rows)
+#    
+#    return numpy.sqrt(photon_flux_p_pixel * quantum_eff * int_time)
+#
+#def read_noise_SNR_counts(c, *params):
+#    quantum_eff, read_noise, dark_current, px_p_columns, px_p_rows, counts_p_sec, int_time = params
+#    
+#    photon_flux_p_pixel = c / (px_p_columns * px_p_rows)
+#    
+#    return photon_flux_p_pixel * px_p_columns * quantum_eff * int_time / read_noise
+#
+#def shot_noise_SNR_time(t, *params):
+#    quantum_eff, read_noise, dark_current, px_p_columns, px_p_rows, counts_p_sec, int_time = params
+#    
+#    photon_flux_p_pixel = counts_p_sec / (px_p_columns * px_p_rows)
+#    
+#    return numpy.sqrt(photon_flux_p_pixel * quantum_eff * t )
+#
+#def read_noise_SNR_time(t, *params):
+#    quantum_eff, read_noise, dark_current, px_p_columns, px_p_rows, counts_p_sec, int_time = params
+#    
+#    photon_flux_p_pixel = counts_p_sec / (px_p_columns * px_p_rows)
+#    
+#    return photon_flux_p_pixel * px_p_columns * quantum_eff * t / read_noise
     
 
 # %%
@@ -95,12 +95,9 @@ time_fig, ax = plt.subplots(1, 1, figsize=(10, 8))
 #
 ## Integration time
 ax.plot(int_time_linspace, SN_t(int_time_linspace, *horiba_BIDD), 'r--', label = 'Horiba BIDD')
-#ax.plot(int_time_linspace, SN_t(int_time_linspace, *horiba_BI), label = 'Horiba BI')
-#ax.plot(int_time_linspace, SN_t(int_time_linspace, *horiba_OE), label = 'Horiba OE')
+ax.plot(int_time_linspace, SN_t(int_time_linspace, *horiba_BI), label = 'Horiba BI')
+ax.plot(int_time_linspace, SN_t(int_time_linspace, *horiba_OE), label = 'Horiba OE')
 #
-
-ax.plot(int_time_linspace, shot_noise_SNR_time(int_time_linspace, *horiba_BIDD), label = 'shot noise limit')
-
 
 ##ax.plot(int_time_linspace, SN_t(int_time_linspace, *andor_BIDD), 'r--', label = 'Andor BIDD')
 ##ax.plot(int_time_linspace, SN_t(int_time_linspace, *andor_BI), label = 'Andor BI')
