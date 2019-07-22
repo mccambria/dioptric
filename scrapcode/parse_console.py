0c450ba33e649201928b8bd69a8d06064d8b0b05
mccambria <kolkowitz@rabi.winnode>
2019-07-22 09:47:40 -0500

    Update parse_console.py

--- scrapcode/parse_console.py
+++ scrapcode/parse_console.py
@@ -11,6 +11,10 @@ Created on Sun Jun 16 11:22:40 2019
 # %% Imports
 
 
+from pathlib import Path
+import numpy
+
+
 # %% Constants
 
 
@@ -20,13 +24,38 @@ Created on Sun Jun 16 11:22:40 2019
 # %% Main
 
 
-def main():
+def main(file_name):
     """When you run the file, we'll call into main, which should contain the
     body of the script.
     """
-
-    pass
-
+    
+    data_dir = Path('C:/Users/kolkowitz/Desktop/iPython_console/')
+    file_name_ext = '{}.txt'.format(file_name)
+    file_path = data_dir / file_name_ext
+    with open(file_path) as file:
+        console = file.read()
+        
+    # Build taus
+    taus = []
+    first_run = console.split('Run index: ')[1]
+    exps = first_run.split('First relaxation time: ')
+    exps = exps[1:]  # ignore the run header
+    for exp in exps:
+        first_time = exp.split('\n\n')[0]
+        taus.append(first_time)
+        second_time = exp.split('Second relaxation time: ')[1].split('\n\n')[0]
+        if second_time != first_time:
+            taus.append(second_time)
+    taus = numpy.array(taus, dtype=numpy.int32)
+    taus = numpy.sort(taus)
+#    print(taus)
+#    return
+    
+        
+    sig_counts = numpy.empty([num_runs, num_steps], dtype=numpy.uint32)
+    sig_counts[:] = numpy.nan
+    ref_counts = numpy.copy(sig_counts)
+    
 
 # %% Run the file
 
@@ -37,7 +66,7 @@ def main():
 if __name__ == '__main__':
 
     # Set up your parameters to be passed to main here
-    file = ''
+    file_name = '2019-07-18_ipython'
     
     # Run the script
-    main(file)
+    main(file_name)
