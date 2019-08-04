48fb261138ad94f33346d3f797e0485ae9fc230c
mccambria <kolkowitz@rabi.winnode>
2019-08-04 12:11:27 -0500

    working changes

--- cfm_control_panel.py
+++ cfm_control_panel.py
@@ -164,10 +164,10 @@ def do_t1_battery(nv_sig, apd_indices):
     # T1 experiment parameters, formatted:
     # [[init state, read state], relaxation_time_range, num_steps, num_reps, num_runs]
     # ~ 19 hours total
-    t1_exp_array = numpy.array([[[States.HIGH, States.LOW],  [0, 100*10**3], 51, int(6.0*10**4), num_runs], # 2.0 hrs, optimize every 3. min, expected error 0.7%
-                                [[States.HIGH, States.LOW],  [0, 700*10**3], 26, int(3.75*10**4), num_runs], # 3.9 hrs, optimize every 5.9 min, expected error 0.9%
-                                [[States.HIGH, States.HIGH],  [0, 100*10**3], 51, int(6.0*10**4), num_runs], # 2.0 hrs, optimize every 3. min, expected error 0.7%
-                                [[States.HIGH, States.HIGH],  [0, 700*10**3], 26, int(3.75*10**4), num_runs], # 3.8 hrs, optimize every 5.9 min, expected error 0.9%
+    t1_exp_array = numpy.array([[[States.HIGH, States.LOW],  [0, 140*10**3], 51, int(6.0*10**4), num_runs], # 2.7 hrs, optimize every 4 min, expected error 0.7%
+                                [[States.HIGH, States.LOW],  [0, 400*10**3], 26, int(3.75*10**4), num_runs], # 2.3 hrs, optimize every 3.5 min, expected error 0.9%
+                                [[States.HIGH, States.HIGH],  [0, 140*10**3], 51, int(6.0*10**4), num_runs], # 2.7 hrs, optimize every 4 min, expected error 0.7%
+                                [[States.HIGH, States.HIGH],  [0, 400*10**3], 26, int(3.75*10**4), num_runs], # 2.3 hrs, optimize every 3.5 min, expected error 0.9%
                                 [[States.ZERO, States.HIGH],  [0, 2*10**6], 26, int(1.5*10**4), num_runs], # 4.4 hrs, optimize every 6.6 min, expected error 1.5%
                                 [[States.ZERO, States.ZERO],  [0, 2*10**6], 26, int(1.5*10**4), num_runs]]) # 4.4 hrs, optimize every 6.6 min, expected error 1.5%
 
