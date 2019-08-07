eca84935002f3fa93544bde37cf5076bd823fd5b
mccambria <kolkowitz@rabi.winnode>
2019-08-07 12:20:54 -0500

    working changes

--- majorroutines/rabi.py
+++ majorroutines/rabi.py
@@ -417,4 +417,4 @@ if __name__ == '__main__':
     #     create_fit_figure(uwave_time_range, num_steps, norm_avg_sig,
     #                       fit_func, popt)
 
-    simulate([0,200], 2.865, 2.87, 0.33, 150)
+    simulate([0,250], 2.9117, 2.9119, 0.57, 150)
