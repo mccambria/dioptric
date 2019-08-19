c81f1593decd16a74522c76882f64edc5d3f6ab1
mccambria <kolkowitz@rabi.winnode>
2019-08-19 18:04:33 -0500

    working changes

--- cfm_control_panel.py
+++ cfm_control_panel.py
@@ -152,7 +152,7 @@ def do_optimize_magnet_angle(nv_sig, apd_indices):
 
 def do_rabi(nv_sig, apd_indices, state):
 
-    uwave_time_range = [0, 250]
+    uwave_time_range = [0, 200]
     num_steps = 51
     num_reps = 2*10**5
     num_runs = 2
@@ -164,14 +164,14 @@ def do_t1_battery(nv_sig, apd_indices):
     
     # T1 experiment parameters, formatted:
     # [[init state, read state], relaxation_time_range, num_steps, num_reps, num_runs]
-    # ~ 4 hours total
-    num_runs = 20
+    # ~ 15 hours total
+    num_runs = 30
     t1_exp_array = numpy.array([[[States.HIGH, States.LOW], [0, 50*10**3], 51, 8*10**4, num_runs],
-                            [[States.HIGH, States.LOW], [0, 150*10**3], 26, 8*10**4, num_runs],
-                            [[States.HIGH, States.HIGH], [0, 50*10**3], 51, 8*10**4, num_runs],
-                            [[States.HIGH, States.HIGH], [0, 150*10**3], 26, 8*10**4, num_runs],
-                            [[States.ZERO, States.HIGH], [0, 150*10**3], 26, 8*10**4, num_runs],
-                            [[States.ZERO, States.ZERO], [0, 150*10**3], 26, 8*10**4, num_runs]])
+                        [[States.HIGH, States.LOW], [0, 700*10**3], 26, 4*10**4, num_runs],
+                        [[States.HIGH, States.HIGH], [0, 50*10**3], 51, 8*10**4, num_runs],
+                        [[States.HIGH, States.HIGH], [0, 700*10**3], 26, 4*10**4, num_runs],
+                        [[States.ZERO, States.HIGH], [0, 1.2*10**6], 26, 2*10**4, num_runs],
+                        [[States.ZERO, States.ZERO], [0, 1.2*10**6], 26, 2*10**4, num_runs]])
     
     # Loop through the experiments
     for exp_ind in range(len(t1_exp_array)):
@@ -341,10 +341,10 @@ if __name__ == '__main__':
     
     nv1_2019_05_10 = {'coords': [0.234, 0.308, 5.06],
       'name': '{}-nv{}_2019_05_10'.format(sample_name, 1),
-      'expected_count_rate': 26,
+      'expected_count_rate': 21,
       'nd_filter': 'nd_1.5',  'pulsed_readout_dur': 510, 'magnet_angle': 109.3,
-      'resonance_LOW':2.64, 'rabi_LOW': 95.6, 'uwave_power_LOW': 9.0,
-      'resonance_HIGH': 3.14, 'rabi_HIGH': 107.2, 'uwave_power_HIGH': 10.0}
+      'resonance_LOW':2.6061, 'rabi_LOW': 186.7, 'uwave_power_LOW': 9.0,
+      'resonance_HIGH': 3.1678, 'rabi_HIGH': 40, 'uwave_power_HIGH': 10.0}
     
     nv_sig_list = [nv1_2019_05_10]
 
@@ -389,14 +389,15 @@ if __name__ == '__main__':
 #            do_pulsed_resonance(nv_sig, apd_indices, freq_center=3.3, freq_range=0.200)
 #            do_pulsed_resonance_state(nv_sig, apd_indices, States.LOW)
 #            do_pulsed_resonance_state(nv_sig, apd_indices, States.HIGH)
-            do_pulsed_resonance(nv_sig, apd_indices,
-                        freq_center=nv_sig['resonance_LOW'], freq_range=0.15)
-            do_pulsed_resonance(nv_sig, apd_indices,
-                        freq_center=nv_sig['resonance_HIGH'], freq_range=0.15)
+#            do_pulsed_resonance(nv_sig, apd_indices,
+#                        freq_center=nv_sig['resonance_LOW'], freq_range=0.15)
+#            do_pulsed_resonance(nv_sig, apd_indices,
+#                        freq_center=nv_sig['resonance_HIGH'], freq_range=0.1)
 #            do_pulsed_resonance(nv_sig, apd_indices, freq_center=2.7, freq_range=0.15)
 #            do_pulsed_resonance(nv_sig, apd_indices, freq_center=3.0, freq_range=0.15)
 #            do_rabi(nv_sig, apd_indices, States.LOW)
 #            do_rabi(nv_sig, apd_indices, States.HIGH)
+            do_t1_battery(nv_sig, apd_indices)
 #            for i in range(5):
 #                fail_bool = find_resonance_and_rabi(nv_sig, apd_indices)
 #                if fail_bool == True:
