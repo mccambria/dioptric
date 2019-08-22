5d2040196951a1e14c0e0577745b915e17e6f5ee
mccambria <kolkowitz@rabi.winnode>
2019-08-22 12:14:56 -0500

    working changes

--- minorroutines/t1_exp_time_estimate.py
+++ minorroutines/t1_exp_time_estimate.py
@@ -60,13 +60,16 @@ def t1_exp_times(exp_array, contrast, exp_count_rate, readout_window):
 
 # %%
 
-num_runs = 30
-t1_exp_array = numpy.array([
-                        [[States.HIGH, States.HIGH], [0, 1.2*10**6], 26, 1.5*10**4, num_runs],
-                        [[States.HIGH, States.LOW], [0, 1.2*10**6], 26, 1.5*10**4, num_runs]
-                        ])
+num_runs = 40
+t1_exp_array = numpy.array([[[States.HIGH, States.LOW], [0, 50*10**3], 51, 8*10**4, num_runs],
+                    [[States.HIGH, States.LOW],  [0, 2*10**6], 26, 1.5*10**4, num_runs],
+                    [[States.HIGH, States.HIGH], [0, 50*10**3], 51, 8*10**4, num_runs],
+                    [[States.HIGH, States.HIGH], [0, 2*10**6], 26, 1.5*10**4, num_runs],
+                    [[States.ZERO, States.HIGH], [0, 1.2*10**6], 26, 2*10**4, num_runs],
+                    [[States.ZERO, States.ZERO], [0, 1.2*10**6], 26, 2*10**4, num_runs]
+                    ])
 
-contrast = .25
+contrast = .35 * 2/3
 exp_count_rate = 21 # kcps
 readout_window = 510 # ns
 
