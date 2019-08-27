435b7e93a77a7d242d2b09b5815e57a6c5430aeb
mccambria <kolkowitz@rabi.winnode>
2019-08-27 15:36:55 -0500

    analysis

--- analysis/relaxation_rate_binning_error.py
+++ analysis/relaxation_rate_binning_error.py
@@ -47,7 +47,7 @@ def exp_eq_offset(t, rate, amp, offset):
 
 # %% Main
     
-def main(folder_name, num_bins, omega = None, omega_unc = None, save_data = True, offset = True):
+def main(folder_name, num_bins, omega = None, omega_ste = None, save_data = True, offset = True):
     
     print('Number of bins: {}'.format(num_bins))
 
@@ -79,7 +79,9 @@ def main(folder_name, num_bins, omega = None, omega_unc = None, save_data = True
     plus_minus_bool = False
     
     zero_relaxation_count_list = []
+    zero_relaxation_count_ste_list = []
     plus_relaxation_count_list = []
+    plus_relaxation_count_ste_list = []
     zero_zero_time_list = []
     plus_plus_time_list = []
     
@@ -95,7 +97,10 @@ def main(folder_name, num_bins, omega = None, omega_unc = None, save_data = True
     g_amp_list = []
     g_offset_list = []
     
+    omega_list = []
+    omega_ste_list = []
     gamma_list =[]
+    gamma_ste_list = []
     # %% Unpack the data
     
     # Unpack the data and sort into arrays. This allows multiple experiments of 
@@ -274,18 +279,27 @@ def main(folder_name, num_bins, omega = None, omega_unc = None, save_data = True
         g_fit_failed = False
         
         #Fit to the (0,0) - (0,1) data to find Omega if we have note passed in an omega value
-        if omega is None and omega_unc is None:
+        if omega is None and omega_ste is None:
             zero_zero_counts_slice = \
                 numpy.average(zero_zero_counts[i:i+slice_size, ::], axis = 0)
+            zero_zero_counts_slice_std = \
+                numpy.std(zero_zero_counts[i:i+slice_size, ::], axis = 0)
             
             zero_plus_counts_slice = \
                 numpy.average(zero_plus_counts[i:i+slice_size, ::], axis = 0)
+            zero_plus_counts_slice_std = \
+                numpy.std(zero_plus_counts[i:i+slice_size, ::], axis = 0)
+                
             # Define the counts for the zero relaxation equation
             zero_relaxation_counts =  zero_zero_counts_slice - zero_plus_counts_slice
+            zero_relaxation_std = numpy.sqrt(zero_zero_counts_slice_std**2 + zero_plus_counts_slice_std**2)
+            zero_relaxation_ste = zero_relaxation_std / numpy.sqrt(slice_size)
             
             #save all the data for possible replotting later
             zero_relaxation_count_list.append(zero_relaxation_counts.tolist())
+            zero_relaxation_count_ste_list.append(zero_relaxation_ste)
             zero_zero_time_list.append(zero_zero_time.tolist())
+            
             
             init_params_list = [1.0, 0.4]
         
@@ -294,19 +308,29 @@ def main(folder_name, num_bins, omega = None, omega_unc = None, save_data = True
                     init_params_list.append(0)
                     init_params = tuple(init_params_list)
                     omega_opti_params, cov_arr = curve_fit(exp_eq_offset, zero_zero_time,
-                                                 zero_relaxation_counts, p0 = init_params)
+                                                 zero_relaxation_counts, p0 = init_params,
+                                                 sigma = zero_relaxation_std, 
+                                                 absolute_sigma=True)
                     
                 else: 
                     init_params = tuple(init_params_list)
                     omega_opti_params, cov_arr = curve_fit(exp_eq, zero_zero_time,
-                                                 zero_relaxation_counts, p0 = init_params)
-               
+                                                 zero_relaxation_counts, p0 = init_params,
+                                                 sigma = zero_relaxation_std, 
+                                                 absolute_sigma=True)
+                
             except Exception:
                 
                 o_fit_failed = True
                 o_fit_failed_list.append(o_fit_failed)
     
             if not o_fit_failed:
+                omega = omega_opti_params[0] / 3
+                omega_ste = numpy.sqrt(cov_arr[0,0]) / 3.0
+                
+                omega_list.append(omega)
+                omega_ste_list.append(omega_ste)
+                            
                 o_fit_failed_list.append(o_fit_failed)
                 
                 o_rate_list.append(omega_opti_params[0])
@@ -316,32 +340,25 @@ def main(folder_name, num_bins, omega = None, omega_unc = None, save_data = True
 
 # %% Fit to the (1,1) - (1,-1) data to find Gamma, only if Omega waas able
 # to fit
-        
-#        plus_plus_avg_sig_counts = \
-#            numpy.average(plus_plus_sig_counts[i:i+slice_size, ::], axis=0)
-#        plus_plus_avg_ref = \
-#            numpy.average(plus_plus_ref_counts[i:i+slice_size, ::])
-#        
-#        plus_plus_norm_avg_sig = plus_plus_avg_sig_counts / plus_plus_avg_ref
-#        
-#        plus_minus_avg_sig_counts = \
-#            numpy.average(plus_minus_sig_counts[i:i+slice_size, ::], axis=0)
-#        plus_minus_avg_ref = \
-#            numpy.average(plus_minus_ref_counts[i:i+slice_size, ::])
-#        
-#        plus_minus_norm_avg_sig = plus_minus_avg_sig_counts / plus_minus_avg_ref
 
         plus_plus_counts_slice = \
             numpy.average(plus_plus_counts[i:i+slice_size, ::], axis = 0)
+        plus_plus_counts_slice_std = \
+            numpy.std(plus_plus_counts[i:i+slice_size, ::], axis = 0)
             
         plus_minus_counts_slice = \
             numpy.average(plus_minus_counts[i:i+slice_size, ::], axis = 0)
+        plus_minus_counts_slice_std = \
+            numpy.std(plus_minus_counts[i:i+slice_size, ::], axis = 0)
         
-        # Define the counts for the plus relaxation equation
+        # Define the counts for the plus relaxation equation, propegate error
         plus_relaxation_counts =  plus_plus_counts_slice - plus_minus_counts_slice
+        plus_relaxation_std = numpy.sqrt(plus_plus_counts_slice_std**2 + plus_minus_counts_slice_std**2)
+        plus_relaxation_ste = plus_relaxation_std / numpy.sqrt(slice_size)
         
         #save all the data for possible replotting later
         plus_relaxation_count_list.append(plus_relaxation_counts.tolist())
+        plus_relaxation_count_ste_list.append(plus_relaxation_ste)
         plus_plus_time_list.append(plus_plus_time.tolist())
         
     
@@ -352,19 +369,28 @@ def main(folder_name, num_bins, omega = None, omega_unc = None, save_data = True
                 init_params = tuple(init_params_list)
                 gamma_opti_params, cov_arr = curve_fit(exp_eq_offset,
                                  plus_plus_time, plus_relaxation_counts,
-                                 p0 = init_params)
+                                 p0 = init_params, sigma = plus_relaxation_std, 
+                                 absolute_sigma=True)
+                    
                 
             else:
                 init_params = tuple(init_params_list)
                 gamma_opti_params, cov_arr = curve_fit(exp_eq,
                                  plus_plus_time, plus_relaxation_counts,
-                                 p0 = init_params)
+                                 p0 = init_params, sigma = plus_relaxation_std, 
+                                 absolute_sigma=True)
 
         except Exception:
             g_fit_failed = True
             g_fit_failed_list.append(g_fit_failed)
             
         if not g_fit_failed:
+            gamma = (gamma_opti_params[0] - omega) / 2
+            gamma_ste = 0.5 * numpy.sqrt(cov_arr[0,0]+omega_ste**2)
+            
+            gamma_list.append(gamma)
+            gamma_ste_list.append(gamma_ste)
+            
             g_fit_failed_list.append(g_fit_failed)
               
             g_rate_list.append(gamma_opti_params[0])
@@ -374,20 +400,18 @@ def main(folder_name, num_bins, omega = None, omega_unc = None, save_data = True
         
         # plot data if saving the data
         if save_data:
-            if omega is None:
-                omega = omega_opti_params[0] / 3
-            gamma = (gamma_opti_params[0] - omega) / 2
-            
-            gamma_list.append(gamma)
             
             fig, ax = plt.subplots(1, 1, figsize=(10, 8))
             plus_time_linspace = numpy.linspace(0, plus_plus_time[-1], num=1000)
             #    ax = axes_pack[1]
-            ax.plot(plus_plus_time, plus_relaxation_counts, 'bo')
+            ax.errorbar(plus_plus_time, plus_relaxation_counts,                         
+                    yerr = plus_relaxation_ste, 
+                    label = 'data', fmt = 'o', color = 'blue')
             if offset:
                 ax.plot(plus_time_linspace,
                     exp_eq_offset(plus_time_linspace, *gamma_opti_params),
                     'r', label = 'fit')
+                
             else:
                 ax.plot(plus_time_linspace,
                     exp_eq(plus_time_linspace, *gamma_opti_params),
@@ -396,7 +420,7 @@ def main(folder_name, num_bins, omega = None, omega_unc = None, save_data = True
             ax.set_ylabel('Normalized signal Counts')
             ax.set_title('(+1,+1) - (+1,-1)')
             ax.legend()
-            text = r'$\gamma = $ {} kHz'.format('%.2f'%gamma)
+            text = r'$\gamma = $ {}$\pm${}kHz'.format('%.2f'%gamma, '%.2f'%gamma_ste)
             
             props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
             ax.text(0.55, 0.95, text, transform=ax.transAxes, fontsize=12,
@@ -404,12 +428,12 @@ def main(folder_name, num_bins, omega = None, omega_unc = None, save_data = True
         # Advance_ the index
         i = i + bin_size
     
-    if omega is None and omega_unc is None:
+    if omega is None and omega_ste is None:
         o_average = numpy.average(o_rate_list)
         o_stdev = numpy.std(o_rate_list)
     else:
         o_average = omega * 3
-        o_stdev = omega_unc * 3
+        o_stdev = omega_ste * 3
     
     g_average = numpy.average(g_rate_list)
     g_stdev = numpy.std(g_rate_list)
@@ -430,6 +454,14 @@ def main(folder_name, num_bins, omega = None, omega_unc = None, save_data = True
                     'num_runs': num_runs,
                     'num_bins': num_bins,
                     'bin_size': bin_size,
+                    'omega_list': omega_list,
+                    'omega_list-units': 'kHz',
+                    'omega_ste_list': omega_ste_list,
+                    'omega_ste_list-units': 'kHz',
+                    'gamma_list': gamma_list,
+                    'gamma_list-units': 'kHz',
+                    'gamma_ste_list': gamma_ste_list,
+                    'gamma_ste_list-units': 'kHz',
                     'o_fit_failed_list': o_fit_failed_list,
                     'g_fit_failed_list': g_fit_failed_list,
                     'o_average': o_average,
@@ -448,8 +480,6 @@ def main(folder_name, num_bins, omega = None, omega_unc = None, save_data = True
                     'o_offset_list-units': 'arb',
                     'g_rate_list': g_rate_list,
                     'g_rate_list-units': 'kHz',
-                    'gamma_list': gamma_list,
-                    'gamma_list-units': 'kHz',
                     'g_amp_list': g_amp_list,
                     'g_amp_list-units': 'arb',
                     'g_offset_list': g_offset_list,
@@ -478,5 +508,5 @@ if __name__ == '__main__':
     folder = 'nv1_2019_05_10_28MHz_4'
 
     
-    main(folder, 9,  1.6, 0.6, True,  True)
+    main(folder, 1,  1.6, 0.6, True,  True)
 
