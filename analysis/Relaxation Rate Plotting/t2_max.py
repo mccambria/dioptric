be6a007649babd5c9e2e7e30aafcb594f497e465
GardillA <gardill@wisc.edu>
2019-10-20 09:24:16 -0500

    figure making

--- analysis/Relaxation Rate Plotting/t2_max.py
+++ analysis/Relaxation Rate Plotting/t2_max.py
@@ -21,15 +21,21 @@ import matplotlib.ticker as ticker
 
 # %%
 
-font_size = 55
+font_size = 75
+marker_size = 30
+
+xlim = [-50,1080]
+ylim = [0.011,2.1]
+#xticks = [0,400, 800]
+xticks = [0,500, 1000]
 
 # %%
 
-nv1_splitting_list = [ 19.5, 19.8, 27.7, 28.9, 41.9, 32.7, 51.8, 97.8, 116, 268, 563.6, 1016.8]
-nv1_omega_avg_list = numpy.array([ 0.83, 1.23, 1.30,  1.000, 1.11, 1.42, 1.85, 1.41, 1.18, 1.04, 1.19, 0.58])
-nv1_omega_error_list = numpy.array([ 0.04, 0.04, 0.06, 0.016, 0.05, 0.05, 0.08, 0.05, 0.06, 0.04, 0.06, 0.03])*2
-nv1_gamma_avg_list = numpy.array([58.3, 117, 64.5, 56.4, 23.5, 42.6, 13.1, 3.91, 4.67, 1.98, 0.70, 0.41])
-nv1_gamma_error_list = numpy.array([1.4, 4, 1.4, 1.3, 0.5, 0.9, 0.2, 0.1, 0.11, 0.1, 0.05, 0.05])*2
+nv1_splitting_list = [ 19.5, 19.8, 27.7, 28.9, 32.7, 51.8, 97.8, 116, 268, 350, 561.7, 1016.8]
+nv1_omega_avg_list = numpy.array([ 0.83, 1.23, 1.30,  1.000,  1.42, 1.85, 1.41, 1.18, 1.04, 0.72, 1.19, 0.58])
+nv1_omega_error_list = numpy.array([ 0.04, 0.04, 0.06, 0.016,  0.05, 0.08, 0.05, 0.06, 0.04, 0.04, 0.06, 0.03])*2
+nv1_gamma_avg_list = numpy.array([58.3, 117, 64.5, 56.4,  42.6, 13.1, 3.91, 4.67, 1.98, 1.57, 0.70, 0.41])
+nv1_gamma_error_list = numpy.array([1.4, 4, 1.4, 1.3,  0.9, 0.2, 0.1, 0.11, 0.1, 0.12, 0.05, 0.05])*2
 
 # The data
 nv2_splitting_list = [15.3,29.2, 45.5, 85.2, 280.4,697.4, 29.1, 44.8, 56.2, 56.9,  101.6]
@@ -45,18 +51,19 @@ nv16_gamma_avg_list = numpy.array([108, 90, 26.2, 17.5, 11.3, 5.6, 3.7, 2.8])
 nv16_gamma_error_list = numpy.array([10, 5, 0.9, 0.6, 0.4, 0.3, 0.4, 0.3])*2
 
 #NV0
-splitting_list = [23.4, 26.3, 36.2, 48.1, 60.5, 92.3, 150.8, 329.6, 884.9, 1080.5, 1148.4]
-omega_avg_list = numpy.array([0.283, 0.33,0.32,  0.314, 0.24, 0.253, 0.29, 0.33, 0.29, 0.28, 0.38])
-omega_error_list = numpy.array([0.017, 0.03,0.03,  0.01, 0.02, 0.012, 0.02, 0.02, 0.02, 0.05, 0.04])*2
-gamma_avg_list = numpy.array([	34.5, 29.0, 20.4,  15.8, 9.1, 6.4, 4.08, 1.23, 0.45, 0.69, 0.35])
-gamma_error_list = numpy.array([1.3, 1.1, 0.5, 0.3, 0.3, 0.1, 0.15, 0.07, 0.03, 0.12, 0.03])*2
+splitting_list = [23.4, 26.3, 36.2, 48.1, 60.5, 92.3, 150.8, 329.6, 884.9] #, 1080.5, 1148.4
+omega_avg_list = numpy.array([0.283, 0.33,0.32,  0.314, 0.24, 0.253, 0.29, 0.33, 0.29])#, 0.28, 0.38
+omega_error_list = numpy.array([0.017, 0.03,0.03,  0.01, 0.02, 0.012, 0.02, 0.02, 0.02])*2 #, 0.05, 0.04
+gamma_avg_list = numpy.array([	34.5, 29.0, 20.4,  15.8, 9.1, 6.4, 4.08, 1.23, 0.45]) #, 0.69, 0.35
+gamma_error_list = numpy.array([1.3, 1.1, 0.5, 0.3, 0.3, 0.1, 0.15, 0.07, 0.03])*2#, 0.12, 0.03
 
 
-nv13_splitting_list = [23.1,  29.8, 51.9, 72.4, 112.9, 164.1]
-nv13_omega_avg_list = numpy.array([1.01, 1.01, 0.39, 0.76, 0.92, 0.66])
-nv13_omega_error_list = numpy.array([0.16,   0.09, 0.04, 0.1, 0.14, 0.11])*2
-nv13_gamma_avg_list = numpy.array([62, 19.3, 17.7, 16.2, 12.1, 5.6])
-nv13_gamma_error_list = numpy.array([8,   1.1, 1.4, 1.1, 0.9, 0.5])*2
+nv13_splitting_list = [10.9,  23.1,  29.8, 51.9, 72.4, 112.9, 164.1, 256.2]
+nv13_omega_avg_list = numpy.array([0.45,  1.01, 1.01, 0.39, 0.76, 0.92, 0.66, 0.23])
+nv13_omega_error_list = numpy.array([0.06,  0.16,   0.09, 0.04, 0.1, 0.14, 0.11, 0.04])*2
+nv13_gamma_avg_list = numpy.array([240,  62, 19.3, 17.7, 16.2, 12.1, 5.6, 2.1])
+nv13_gamma_error_list = numpy.array([25,  8,   1.1, 1.4, 1.1, 0.9, 0.5, 0.3])*2
+
 # %% NV1
 
 nv1_color = '#87479b'
@@ -125,17 +132,17 @@ average_traditional_t2_error_0= numpy.empty([1000])
 average_traditional_t2_error_0[:]= numpy.sqrt(sum(T2_max_traditional_error_0**2)) / len(T2_max_traditional_error_0)
 
 
-print(T2_max_1[1]*1000, T2_max_error_1[1]*2000, 
-      T2_max_2[0]*1000, T2_max_error_2[0]*2000,
-      T2_max_16[0]*1000, T2_max_error_16[0]*2000,
-      T2_max_0[0]*1000, T2_max_error_0[0]*2000)
+print(T2_max_1[1]*1000, T2_max_error_1[1]*1000, 
+      T2_max_2[0]*1000, T2_max_error_2[0]*1000,
+      T2_max_16[0]*1000, T2_max_error_16[0]*1000,
+      T2_max_0[0]*1000, T2_max_error_0[0]*1000)
 
 # %% NV13
 
 T2_max_13 = 2 / (3 * nv13_omega_avg_list + nv13_gamma_avg_list) 
 T2_max_error_13 = (3*nv13_omega_avg_list + nv13_gamma_avg_list)**-2 * numpy.sqrt( (6*nv13_omega_error_list)**2 + (2*nv13_gamma_error_list)**2 )
 
-print(T2_max_13[0]*1000, T2_max_error_13[0]*2000)
+print(T2_max_13[0]*1000, T2_max_error_13[0]*1000)
 # %%
 
 linspace = numpy.linspace(0, 2000, 1000)
@@ -144,7 +151,7 @@ linspace = numpy.linspace(0, 2000, 1000)
 fig1, ax = plt.subplots(1, 1, figsize=(8, 8))
 
 ax.errorbar(nv1_splitting_list, T2_max_1, yerr = T2_max_error_1, 
-            label = 'NV1',  color= nv1_color, fmt='D',markersize = 20, elinewidth=4)
+            label = 'NV1',  color= nv1_color, fmt='D',markersize = marker_size, elinewidth=4)
 
 print(numpy.average(T2_max_traditional_1))
 ax.set_yscale("log", nonposy='clip')
@@ -156,12 +163,13 @@ ax.tick_params(which = 'both', length=14, width=3, colors='k',
                 grid_alpha=0.7, labelsize = font_size)
 
 ax.tick_params(which = 'major', length=20, width=5)
-ax.set_xticks([0,500, 1000])
-
+ax.set_xticks(xticks)
+ax.set_xlim(xlim)
+ax.set_ylim(ylim)
 
 ax.grid(axis='y',lw=3)
 
-plt.xlabel('Splitting (MHz)', fontsize=font_size)
+plt.xlabel(r'$\Delta_{\pm}$ (MHz)', fontsize=font_size)
 plt.ylabel(r'$T_{2,max}$ (ms)', fontsize=font_size)
 
 fig1.savefig("C:/Users/Aedan/Creative Cloud Files/Paper Illustrations/Magnetically Forbidden Rate/fig_3a_inset.pdf", bbox_inches='tight')
@@ -172,7 +180,7 @@ fig1.savefig("C:/Users/Aedan/Creative Cloud Files/Paper Illustrations/Magnetical
 fig2, ax = plt.subplots(1, 1, figsize=(8, 8))
 
 ax.errorbar(nv2_splitting_list, T2_max_2, yerr = T2_max_error_2, 
-            label = 'NV2',  color= nv2_color, fmt='D',markersize = 20, elinewidth=4)
+            label = 'NV2',  color= nv2_color, fmt='D',markersize = marker_size, elinewidth=4)
 #ax.plot(linspace, average_traditional_t2_max_2,  
 #            label = 'NV2', linestyle='-.', color = nv2_color, linewidth=3)
 
@@ -185,10 +193,12 @@ ax.tick_params(which = 'major', length=20, width=5)
 
 ax.set_yscale("log", nonposy='clip')
 ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y,pos: ('{{:.{:1d}f}}'.format(int(numpy.maximum(-numpy.log10(y),0)))).format(y)))
-ax.set_xticks([0,250, 500, 750])
+ax.set_xticks(xticks)
+ax.set_xlim(xlim)
+ax.set_ylim(ylim)
 
 ax.grid(axis='y',lw=3)
-plt.xlabel('Splitting (MHz)', fontsize=font_size)
+plt.xlabel(r'$\Delta_{\pm}$ (MHz)', fontsize=font_size)
 plt.ylabel(r'$T_{2,max}$ (ms)', fontsize=font_size)
 
 fig2.savefig("C:/Users/Aedan/Creative Cloud Files/Paper Illustrations/Magnetically Forbidden Rate/fig_3b_inset.pdf", bbox_inches='tight')
@@ -197,7 +207,7 @@ fig2.savefig("C:/Users/Aedan/Creative Cloud Files/Paper Illustrations/Magnetical
 # %%
 fig3, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.errorbar(nv16_splitting_list, T2_max_16, yerr = T2_max_error_16, 
-            label = 'NV16',  color= nv16_color, fmt='D',markersize = 20, elinewidth=4)
+            label = 'NV16',  color= nv16_color, fmt='D',markersize = marker_size, elinewidth=4)
 #ax.plot(linspace, average_traditional_t2_max_16,  
 #            label = 'NV16', linestyle='--', color = nv16_color, linewidth=3)
 
@@ -210,14 +220,14 @@ ax.tick_params(which = 'major', length=20, width=5)
 
 ax.set_yscale("log", nonposy='clip')
 ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y,pos: ('{{:.{:1d}f}}'.format(int(numpy.maximum(-numpy.log10(y),0)))).format(y)))
-ax.set_xticks([0,250, 500, 750])
+ax.set_xticks(xticks)
 
 ax.grid(axis='y',lw=3)
-plt.xlabel('Splitting (MHz)', fontsize=font_size)
+plt.xlabel(r'$\Delta_{\pm}$ (MHz)', fontsize=font_size)
 plt.ylabel(r'$T_{2,max}$ (ms)', fontsize=font_size)
 
-#ax.set_xlim([-20,1100])
-#ax.set_ylim([-0.1,2.3])
+ax.set_xlim(xlim)
+ax.set_ylim(ylim)
 
 #ax.set_xscale("log", nonposx='clip')
 
@@ -229,7 +239,7 @@ fig3.savefig("C:/Users/Aedan/Creative Cloud Files/Paper Illustrations/Magnetical
 # %%
 fig4, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.errorbar(splitting_list, T2_max_0, yerr = T2_max_error_0, 
-            label = 'NV0',  color= nv0_color, fmt='D',markersize = 20, elinewidth=4)
+            label = 'NV0',  color= nv0_color, fmt='D',markersize = marker_size, elinewidth=4)
 
 print(numpy.average(T2_max_traditional_0))
 
@@ -240,14 +250,14 @@ ax.tick_params(which = 'major', length=20, width=5)
 
 ax.set_yscale("log", nonposy='clip')
 ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y,pos: ('{{:.{:1d}f}}'.format(int(numpy.maximum(-numpy.log10(y),0)))).format(y)))
-ax.set_xticks([0,500, 1000])
+ax.set_xticks(xticks)
 
 ax.grid(axis='y',lw=3)
-plt.xlabel('Splitting (MHz)', fontsize=font_size)
+plt.xlabel(r'$\Delta_{\pm}$ (MHz)', fontsize=font_size)
 plt.ylabel(r'$T_{2,max}$ (ms)', fontsize=font_size)
 
-#ax.set_xlim([-20,1100])
-#ax.set_ylim([-0.1,2.3])
+ax.set_xlim(xlim)
+ax.set_ylim(ylim)
 
 #ax.set_xscale("log", nonposx='clip')
 
@@ -260,7 +270,7 @@ fig4.savefig("C:/Users/Aedan/Creative Cloud Files/Paper Illustrations/Magnetical
 # %%
 fig5, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.errorbar(nv13_splitting_list, T2_max_13, yerr = T2_max_error_13, 
-            label = 'NV0',  color= nv0_color, fmt='D',markersize = 20, elinewidth=4)
+            label = 'NV0',  color= nv0_color, fmt='D',markersize = marker_size, elinewidth=4)
 
 #print(numpy.average(T2_max_traditional_13))
 
@@ -271,14 +281,14 @@ ax.tick_params(which = 'major', length=20, width=5)
 
 ax.set_yscale("log", nonposy='clip')
 ax.yaxis.set_major_formatter(ticker.FuncFormatter(lambda y,pos: ('{{:.{:1d}f}}'.format(int(numpy.maximum(-numpy.log10(y),0)))).format(y)))
-ax.set_xticks([0,50, 100, 150])
+ax.set_xticks([0, 150, 300])
 
 ax.grid(axis='y', lw=3)
-plt.xlabel('Splitting (MHz)', fontsize=font_size)
+plt.xlabel(r'$\Delta_{\pm}$ (MHz)', fontsize=font_size)
 plt.ylabel(r'$T_{2,max}$ (ms)', fontsize=font_size)
 
-#ax.set_xlim([-20,1100])
-#ax.set_ylim([-0.1,2.3])
+ax.set_xlim([-8,350])
+ax.set_ylim([0.004,1.1])
 
 #ax.set_xscale("log", nonposx='clip')
 
