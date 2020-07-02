7f33f8ecb1cc30515e3b7d5038d97bce810d73af
GardillA <gardill@wisc.edu>
2020-07-02 14:42:37 -0500

    Update charge_ring_plotting.py

--- plotting/charge_ring_plotting.py
+++ plotting/charge_ring_plotting.py
@@ -536,46 +536,49 @@ if __name__ == '__main__':
     radius_50 = [1.5, 3.5, 10.5, 13.6, 15.4, 16.5, 17.4, 19.3, 18.4, 19.4, 20.2,20.5, 23.5,25  ] # um
     radius_50_err =  radius_10_err
     
-#    popt_1s, pcov = curve_fit(exponential, powers, radius_1, p0= (20, 1))
-#    popt_10s, pcov = curve_fit(exponential, powers, radius_10, p0= (20, 1))
-#    popt_50s, pcov = curve_fit(exponential, powers, radius_50, p0= (20, 1))
-#    
-#    power_linspace = numpy.linspace(powers[0], powers[-1], 1000) 
-#
-#    fig, ax = plt.subplots(1,1, figsize = (8, 8))
-#    ax.errorbar(powers, radius_1, xerr = power_err, yerr = radius_1_err, fmt = 'bo', label = '1 s green pulse')
-#    ax.plot(power_linspace, exponential(power_linspace, *popt_1s), 'b-', label = '1 s fit')
-#    ax.errorbar(powers, radius_10, xerr = power_err, yerr = radius_10_err, fmt = 'ro',  label = '10 s green pulse')
-#    ax.plot(power_linspace, exponential(power_linspace, *popt_10s), 'r-', label = '10 s fit')   
-#    ax.errorbar(powers, radius_50, xerr = power_err, yerr = radius_50_err, fmt = 'go',  label = '50 s green pulse')
-#    ax.plot(power_linspace, exponential(power_linspace, *popt_50s), 'g-', label = '50 s fit')
-#
-##    ax.set_xscale('log')
-#    ax.set_xlabel('Green optical power (mW)')
-#    ax.set_ylabel('Charge ring radius (um)')
-#    ax.legend()
-#  
-#    text_eq = r'$A_0 (1- e^{-P/d})$'
-#    
-#    text_1s = '\n'.join(('1 s fit',
-#                      r'$A_0 = {}$ um'.format('%.1f'%popt_1s[0]),
-#                      r'$P = {}$ mW'.format('%.1f'%popt_1s[1])))
-#    text_10s= '\n'.join(('10 s fit',
-#                      r'$A_0 = {}$ um'.format('%.1f'%popt_10s[0]),
-#                      r'$P = {}$ mW'.format('%.2f'%popt_10s[1])))
-#    text_50s = '\n'.join(('50 s fit',
-#                      r'$A_0 = {}$ um'.format('%.1f'%popt_50s[0]),
-#                      r'$P = {}$ mW'.format('%.2f'%popt_50s[1])))  
-#    
-#    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
-#    ax.text(0.3, 0.6, text_eq, transform=ax.transAxes, fontsize=12,
-#            verticalalignment='top', bbox=props)
-#    ax.text(0.3, 0.52, text_1s, transform=ax.transAxes, fontsize=12,
-#            verticalalignment='top', bbox=props)
-#    ax.text(0.3, 0.35, text_10s, transform=ax.transAxes, fontsize=12,
-#            verticalalignment='top', bbox=props)
-#    ax.text(0.3, 0.20, text_50s, transform=ax.transAxes, fontsize=12,
-#            verticalalignment='top', bbox=props)
+    popt_1s, pcov = curve_fit(exponential, powers, radius_1, p0= (20, 1, 5))
+    popt_10s, pcov = curve_fit(exponential, powers, radius_10, p0= (20, 1, 5))
+    popt_50s, pcov = curve_fit(exponential, powers, radius_50, p0= (20, 1, 5))
+    
+    power_linspace = numpy.linspace(powers[0], powers[-1], 1000) 
+
+    fig, ax = plt.subplots(1,1, figsize = (8, 8))
+    ax.errorbar(powers, radius_1, xerr = power_err, yerr = radius_1_err, fmt = 'bo', label = '1 s green pulse')
+    ax.plot(power_linspace, exponential(power_linspace, *popt_1s), 'b-', label = '1 s fit')
+    ax.errorbar(powers, radius_10, xerr = power_err, yerr = radius_10_err, fmt = 'ro',  label = '10 s green pulse')
+    ax.plot(power_linspace, exponential(power_linspace, *popt_10s), 'r-', label = '10 s fit')   
+    ax.errorbar(powers, radius_50, xerr = power_err, yerr = radius_50_err, fmt = 'go',  label = '50 s green pulse')
+    ax.plot(power_linspace, exponential(power_linspace, *popt_50s), 'g-', label = '50 s fit')
+
+#    ax.set_xscale('log')
+    ax.set_xlabel('Green optical power (mW)')
+    ax.set_ylabel('Charge ring radius (um)')
+    ax.legend()
+  
+    text_eq = r'$C + A_0 (1- e^{-P/d})$'
+    
+    text_1s = '\n'.join(('1 s fit',
+                     r'$C = {}$ um'.format('%.1f'%popt_1s[2]),
+                      r'$A_0 = {}$ um'.format('%.1f'%popt_1s[0]),
+                      r'$P = {}$ mW'.format('%.1f'%popt_1s[1])))
+    text_10s= '\n'.join(('10 s fit',
+                     r'$C = {}$ um'.format('%.1f'%popt_10s[2]),
+                      r'$A_0 = {}$ um'.format('%.1f'%popt_10s[0]),
+                      r'$P = {}$ mW'.format('%.2f'%popt_10s[1])))
+    text_50s = '\n'.join(('50 s fit',
+                     r'$C = {}$ um'.format('%.1f'%popt_50s[2]),
+                      r'$A_0 = {}$ um'.format('%.1f'%popt_50s[0]),
+                      r'$P = {}$ mW'.format('%.2f'%popt_50s[1])))  
+    
+    props = dict(boxstyle='round', facecolor='wheat', alpha=0.5)
+    ax.text(0.3, 0.6, text_eq, transform=ax.transAxes, fontsize=12,
+            verticalalignment='top', bbox=props)
+    ax.text(0.3, 0.52, text_1s, transform=ax.transAxes, fontsize=12,
+            verticalalignment='top', bbox=props)
+    ax.text(0.3, 0.35, text_10s, transform=ax.transAxes, fontsize=12,
+            verticalalignment='top', bbox=props)
+    ax.text(0.3, 0.20, text_50s, transform=ax.transAxes, fontsize=12,
+            verticalalignment='top', bbox=props)
     
     # %% Manual data fitting for green time
     
@@ -625,15 +628,15 @@ if __name__ == '__main__':
     text_eq = r'$C + A_0 (1- e^{-t/d})$'
     
     text_4mw = '\n'.join(('0.3 mW fit',
-                      r'$C = {}$ s'.format('%.1f'%popt_4mw[2]),
+                      r'$C = {}$ um'.format('%.1f'%popt_4mw[2]),
                       r'$A_0 = {}$ um'.format('%.1f'%popt_4mw[0]),
                       r'$d = {}$ s'.format('%.1f'%popt_4mw[1])))
     text_8mw = '\n'.join(('0.75 mW fit',
-                      r'$C = {}$ s'.format('%.1f'%popt_8mw[2]),
+                      r'$C = {}$ um'.format('%.1f'%popt_8mw[2]),
                       r'$A_0 = {}$ um'.format('%.1f'%popt_8mw[0]),
                       r'$d = {}$ s'.format('%.2f'%popt_8mw[1])))
     text_12mw = '\n'.join(('1.3 mW fit',
-                      r'$C = {}$ s'.format('%.1f'%popt_12mw[2]),
+                      r'$C = {}$ um'.format('%.1f'%popt_12mw[2]),
                       r'$A_0 = {}$ um'.format('%.1f'%popt_12mw[0]),
                       r'$d = {}$ s'.format('%.2f'%popt_12mw[1])))  
     
