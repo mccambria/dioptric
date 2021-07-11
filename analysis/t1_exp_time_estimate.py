b202ab149cc16ac40da7bd5de42772682a9d36da
agardill <agardill@gmail.com>
2021-07-11 12:26:28 -0500

    working changes

--- analysis/t1_exp_time_estimate.py
+++ analysis/t1_exp_time_estimate.py
@@ -62,12 +62,12 @@ def t1_exp_times(exp_array, contrast, exp_count_rate, readout_window):
 
 # %%
 
-num_runs = 120
+num_runs = 80
 num_reps = 1000
 num_steps = 12
 min_tau = 20e3
-max_tau_omega = int(11.5e6)
-max_tau_gamma = int(8e6)
+max_tau_omega = int(8e6)
+max_tau_gamma = int(6e6)
 t1_exp_array = numpy.array([
         [[States.ZERO, States.HIGH], [min_tau, max_tau_omega], num_steps, num_reps, num_runs],
         [[States.ZERO, States.ZERO], [min_tau, max_tau_omega], num_steps, num_reps, num_runs],
@@ -83,8 +83,8 @@ t1_exp_array = numpy.array([
         [[States.LOW, States.LOW], [min_tau, max_tau_gamma//3], num_steps, num_reps, num_runs],
         ], dtype=object)
 
-contrast = 0.14  # arb
-exp_count_rate = 800  # kcps
+contrast = 0.16  # arb
+exp_count_rate = 1000  # kcps
 readout_window = 350  # ns
 
 t1_exp_times(t1_exp_array, contrast, exp_count_rate, readout_window)
