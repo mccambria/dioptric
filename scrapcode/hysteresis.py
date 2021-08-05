756c63a505ea66abc6f545b884c55deba5b65ea6
mccambria <45080582+mccambria@users.noreply.github.com>
2021-08-05 18:53:59 -0500

    debugging

--- scrapcode/hysteresis.py
+++ scrapcode/hysteresis.py
@@ -8,27 +8,35 @@ Plotting and exploring hysteresis curves
 
 import matplotlib.pyplot as plt
 import numpy as np
+import time
 
 
 def invert_hysteresis(position, prev_turning_position, a, b):
     # The hysteresis curve is p(v) = a * v**2 + b * v
     # We want to feedforward using this curve to set the piezo voltage
-    # such that the nominal voltage passed by the user functions 
+    # such that the nominal voltage passed by the user functions
     # linearly and without hysteresis. The goal is to prevent the
-    # accumulation of small errors until active feedback (eg 
+    # accumulation of small errors until active feedback (eg
     # optimizing on an NV) can be performed
-    
+
     # The adjustment voltage we need is obtained by inverting p(v)
     p = position - prev_turning_position
     abs_p = abs(p)
-    v = (-b + np.sqrt(b**2 + 4 * a * abs_p)) / (2 * a)
-    
+    v = (-b + np.sqrt(b ** 2 + 4 * a * abs_p)) / (2 * a)
+
     # return position
     return prev_turning_position + (np.sign(p) * v)
 
-def hysteresis_curve(voltage, prev_turning_delta, 
-                     prev_turning_voltage, prev_turning_pos,
-                     full_scale_voltage, a, b):
+
+def hysteresis_curve(
+    voltage,
+    prev_turning_delta,
+    prev_turning_voltage,
+    prev_turning_pos,
+    full_scale_voltage,
+    a,
+    b,
+):
 
     # scale = abs(prev_turning_delta / full_scale_voltage)
     # scale = abs(prev_turning_delta / full_scale_voltage)
@@ -36,7 +44,7 @@ def hysteresis_curve(voltage, prev_turning_delta,
     # scaled_diff = diff * scale
     # response = lambda diff : scale * (a * scaled_diff**2 + b * scaled_diff)
     diff = voltage - prev_turning_voltage
-    response = lambda diff : a * diff**2 + b * diff
+    response = lambda diff: a * diff ** 2 + b * diff
 
     return prev_turning_pos + (np.sign(diff) * response(abs(diff)))
 
@@ -53,14 +61,19 @@ def plot_hysteresis(turning_voltages, full_scale_voltage, a, b):
     for turning_voltage in turning_voltages[1:]:
         num_steps = round((turning_voltage - prev_turning_voltage) / step_size)
         num_steps = abs(num_steps)
-        segment_voltages = np.linspace(prev_turning_voltage, turning_voltage, 
-                                       num_steps)
+        segment_voltages = np.linspace(prev_turning_voltage, turning_voltage, num_steps)
         segment_positions = []
         for vol in segment_voltages:
             # vol = invert_hysteresis(vol, prev_turning_voltage, a, b)
-            pos = hysteresis_curve(vol, prev_turning_delta, 
-                                   prev_turning_voltage, prev_turning_pos,
-                                   full_scale_voltage, a, b)
+            pos = hysteresis_curve(
+                vol,
+                prev_turning_delta,
+                prev_turning_voltage,
+                prev_turning_pos,
+                full_scale_voltage,
+                a,
+                b,
+            )
             segment_positions.append(float(pos))
         voltages.extend(segment_voltages)
         positions.extend(segment_positions)
@@ -72,29 +85,35 @@ def plot_hysteresis(turning_voltages, full_scale_voltage, a, b):
 
     fig, ax = plt.subplots()
     ax.plot(voltages, positions)
-    ax.set_xlabel('Applied value')
-    ax.set_ylabel('Actual value')
+    ax.set_xlabel("Applied value")
+    ax.set_ylabel("Actual value")
+
+    # fig.canvas.flush_events()
+    plt.pause(0.001)
+    ax.set_xlabel("test")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+
+    plt.ion()
 
     linear_scale = 4
     full_scale_voltage = linear_scale
     a = 0.055
     # x = a * x**2 + b * x
-    b = (linear_scale - a * linear_scale**2) / linear_scale
+    b = (linear_scale - a * linear_scale ** 2) / linear_scale
     print(b)
-    
+
     exps = [
-        [3,4,3,5,3,6,3,7,3],
+        # [3, 4, 3, 5, 3, 6, 3, 7, 3],
         # [0, 10, 0, 8, 0, 6, 0],  # Loop scale test
-        # [0, 10, 2, 10, 4, 10, 6, 10],  # Reverse loop scale test
-        # [0, 10, 0, 7.5, 2.5, 7.5, 2.5, 7.5],  # Halving the drive
-        # [0, 10, 0, 9, 1, 8, 2, 7, 4],  # Spiral simple
+        [0, 10, 2, 10, 4, 10, 6, 10],  # Reverse loop scale test
+        [0, 10, 0, 7.5, 2.5, 7.5, 2.5, 7.5],  # Halving the drive
+        [0, 10, 0, 9, 1, 8, 2, 7, 4],  # Spiral simple
         # [0, 10, 0, 9, 1, 8, 2, 7, 3, 6, 4, 6],  # Spiral simple 2
         # [0, 10, 0, 5, 4, 10],  # Spiral simple 2
         # [0, 10]
     ]
     for exp in exps:
         plot_hysteresis(exp, full_scale_voltage, a, b)
-    plt.show()
+    plt.show(block=True)
--- servers/outputs/objective_piezo.py
+++ servers/outputs/objective_piezo.py
@@ -36,17 +36,22 @@ from pathlib import Path
 
 
 class ObjectivePiezo(LabradServer):
-    name = 'objective_piezo'
+    name = "objective_piezo"
     pc_name = socket.gethostname()
 
     def initServer(self):
-        filename = 'E:/Shared drives/Kolkowitz Lab Group/nvdata/pc_{}/labrad_logging/{}.log'
+        filename = (
+            "E:/Shared drives/Kolkowitz Lab Group/nvdata/pc_{}/labrad_logging/{}.log"
+        )
         filename = filename.format(self.pc_name, self.name)
-        logging.basicConfig(level=logging.DEBUG, 
-                    format='%(asctime)s %(levelname)-8s %(message)s',
-                    datefmt='%y-%m-%d_%H-%M-%S', filename=filename)
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format="%(asctime)s %(levelname)-8s %(message)s",
+            datefmt="%y-%m-%d_%H-%M-%S",
+            filename=filename,
+        )
         self.task = None
-        
+
     def sub_init_server_z(self):
         """Sub-routine to be called by xyz server"""
         self.z_last_position = None
@@ -57,23 +62,23 @@ class ObjectivePiezo(LabradServer):
 
     async def get_config_z(self):
         p = self.client.registry.packet()
-        p.cd(['', 'Config', 'DeviceIDs'])
-        p.get('objective_piezo_model')
-        p.get('objective_piezo_serial')
-        p.cd(['', 'Config', 'Wiring', 'Daq'])
-        p.get('ao_objective_piezo')
-        p.get('di_clock')
-        p.cd(['', 'Config', 'Positioning'])
-        p.get('z_hysteresis_a')
-        p.get('z_hysteresis_b')
+        p.cd(["", "Config", "DeviceIDs"])
+        p.get("objective_piezo_model")
+        p.get("objective_piezo_serial")
+        p.cd(["", "Config", "Wiring", "Daq"])
+        p.get("ao_objective_piezo")
+        p.get("di_clock")
+        p.cd(["", "Config", "Positioning"])
+        p.get("z_hysteresis_a")
+        p.get("z_hysteresis_b")
         result = await p.send()
-        return result['get']
+        return result["get"]
 
     def on_get_config_z(self, config):
         # Load the generic device
         gcs_dll_path = str(Path.home())
-        gcs_dll_path += '\\Documents\\GitHub\\kolkowitz-nv-experiment-v1.0'
-        gcs_dll_path += '\\servers\\outputs\\GCSTranslator\\PI_GCS2_DLL_x64.dll'
+        gcs_dll_path += "\\Documents\\GitHub\\kolkowitz-nv-experiment-v1.0"
+        gcs_dll_path += "\\servers\\outputs\\GCSTranslator\\PI_GCS2_DLL_x64.dll"
         self.piezo = GCSDevice(devname=config[0], gcsdll=gcs_dll_path)
         # Connect the specific device with the serial number
         self.piezo.ConnectUSB(config[1])
@@ -84,8 +89,8 @@ class ObjectivePiezo(LabradServer):
         self.daq_di_clock = config[3]
         self.z_hysteresis_a = config[4]
         self.z_hysteresis_b = config[5]
-        logging.debug('Init complete')
-        
+        logging.debug("Init complete")
+
     def compensate_hysteresis_z(self, position):
         """
         The hysteresis curve is p(v) = a * v**2 + b * v.
@@ -106,12 +111,12 @@ class ObjectivePiezo(LabradServer):
         float
             Compensated voltage to set
         """
-        
+
         single_value = False
         if type(position) not in [numpy.ndarray, list]:
             single_value = True
             position = [position]
-        
+
         # Pull everything out of self to save some lookup time
         last_position = self.z_last_position
         current_direction = self.z_current_direction
@@ -124,35 +129,35 @@ class ObjectivePiezo(LabradServer):
             last_turning_position = position[0]
         a = self.z_hysteresis_a
         b = self.z_hysteresis_b
-        
-        # We'll have to for loop because the (n+1)th value depends 
+
+        # We'll have to for loop because the (n+1)th value depends
         # on the nth value
         compensated_voltage = []
         for val in position:
-            
+
             # compensated_voltage.append(val)
             # continue
-            
+
             # First determine if we're turning around
             movement_direction = numpy.sign(val - last_position)
             if movement_direction == -current_direction:
                 last_turning_position = last_position
                 current_direction = movement_direction
-            
+
             # The adjustment voltage we need is obtained by inverting p(v)
             abs_p = abs(val - last_turning_position)
             # v = (-b + numpy.sqrt(b**2 + 4 * a * abs_p)) / (2 * a)
             v = abs_p
             result = last_turning_position + (movement_direction * v)
             compensated_voltage.append(result)
-            
+
             # Cache the last position
             last_position = val
-            
+
         self.z_last_position = val
         self.z_current_direction = movement_direction
         self.z_last_turning_position = last_turning_position
-        
+
         if single_value:
             return compensated_voltage[0]
         else:
@@ -163,10 +168,10 @@ class ObjectivePiezo(LabradServer):
         # Close the existing task if there is one
         if self.task is not None:
             self.close_task_internal()
-            
+
         # Make sure the voltages are an array
         voltages = numpy.array(voltages)
-            
+
         compensated_voltages = self.compensate_hysteresis_z(voltages)
 
         # Write the initial voltages and stream the rest
@@ -181,8 +186,9 @@ class ObjectivePiezo(LabradServer):
         self.task = task
 
         # Set up the output channels
-        task.ao_channels.add_ao_voltage_chan(self.daq_ao_objective_piezo,
-                                             min_val=3.0, max_val=7.0)
+        task.ao_channels.add_ao_voltage_chan(
+            self.daq_ao_objective_piezo, min_val=3.0, max_val=7.0
+        )
 
         # Set up the output stream
         output_stream = nidaqmx.task.OutStream(task)
@@ -191,9 +197,10 @@ class ObjectivePiezo(LabradServer):
         # Configure the sample to advance on the rising edge of the PFI input.
         # The frequency specified is just the max expected rate in this case.
         # We'll stop once we've run all the samples.
-        freq = float(1/(period*(10**-9)))  # freq in seconds as a float
-        task.timing.cfg_samp_clk_timing(freq, source=self.daq_di_clock,
-                                        samps_per_chan=num_stream_voltages)
+        freq = float(1 / (period * (10 ** -9)))  # freq in seconds as a float
+        task.timing.cfg_samp_clk_timing(
+            freq, source=self.daq_di_clock, samps_per_chan=num_stream_voltages
+        )
 
         writer.write_many_sample(stream_voltages)
 
@@ -202,15 +209,14 @@ class ObjectivePiezo(LabradServer):
 
         task.start()
 
-    def close_task_internal(self, task_handle=None, status=None,
-                            callback_data=None):
+    def close_task_internal(self, task_handle=None, status=None, callback_data=None):
         task = self.task
         if task is not None:
             task.close()
             self.task = None
         return 0
 
-    @setting(22, voltage='v[]')
+    @setting(22, voltage="v[]")
     def write_z(self, c, voltage):
         """Write the specified voltage to the piezo"""
 
@@ -224,25 +230,25 @@ class ObjectivePiezo(LabradServer):
 
         with nidaqmx.Task() as task:
             # Set up the output channels
-            task.ao_channels.add_ao_voltage_chan(self.daq_ao_objective_piezo,
-                                                 min_val=3.0, max_val=7.0)
+            task.ao_channels.add_ao_voltage_chan(
+                self.daq_ao_objective_piezo, min_val=3.0, max_val=7.0
+            )
             task.write(compensated_voltage)
 
-    @setting(21, returns='v[]')
+    @setting(21, returns="v[]")
     def read_z(self, c):
         """Return the current voltages on the piezo's DAQ channel"""
         with nidaqmx.Task() as task:
             # Set up the internal channels - to do: actual parsing...
-            if self.daq_ao_objective_piezo == 'dev1/AO2':
-                chan_name = 'dev1/_ao2_vs_aognd'
-            task.ai_channels.add_ai_voltage_chan(chan_name,
-                                                 min_val=3.0, max_val=7.0)
+            if self.daq_ao_objective_piezo == "dev1/AO2":
+                chan_name = "dev1/_ao2_vs_aognd"
+            task.ai_channels.add_ai_voltage_chan(chan_name, min_val=3.0, max_val=7.0)
             voltage = task.read()
         return voltage
-    
-    
-    @setting(23, center='v[]', scan_range='v[]',
-             num_steps='i', period='i', returns='*v[]')
+
+    @setting(
+        23, center="v[]", scan_range="v[]", num_steps="i", period="i", returns="*v[]"
+    )
     def load_scan_z(self, c, center, scan_range, num_steps, period):
         """Load a linear sweep with the DAQ"""
 
@@ -250,22 +256,25 @@ class ObjectivePiezo(LabradServer):
         low = center - half_scan_range
         high = center + half_scan_range
         voltages = numpy.linspace(low, high, num_steps)
-        self.load_stream_writer_z(c, 'ObjectivePiezo-load_scan_z',
-                                  voltages, period)
+        self.load_stream_writer_z(c, "ObjectivePiezo-load_scan_z", voltages, period)
         return voltages
-    
+
     # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    @setting(24, z_voltages='*v[]', period='i', returns='*v[]')
+    @setting(24, z_voltages="*v[]", period="i", returns="*v[]")
     def load_arb_scan_z(self, c, z_voltages, period):
         """Load a list of voltages with the DAQ"""
 
-        self.load_stream_writer_z(c, 'ObjectivePiezo-load_arb_scan_z',
-                                  numpy.array(z_voltages), period)
+        self.load_stream_writer_z(
+            c, "ObjectivePiezo-load_arb_scan_z", numpy.array(z_voltages), period
+        )
         return z_voltages
+
     # +++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 
 __server__ = ObjectivePiezo()
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     from labrad import util
+
     util.runServer(__server__)
