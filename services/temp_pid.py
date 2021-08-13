c1b86a3306526f9be596f31742bf110a8280b10b
mccambria <45080582+mccambria@users.noreply.github.com>
2021-08-13 10:00:19 -0500

    Update temp_pid.py

--- services/temp_pid.py
+++ services/temp_pid.py
@@ -38,7 +38,7 @@ def calc_error(run_params, actual):
         time_history.pop(0)
 
 
-def pid(run_params, actual):
+def pid(state, pid_coeffs, actual):
     """Returns the power to set given the current actual temperature 
     measurement and the target temperature
 
@@ -55,37 +55,18 @@ def pid(run_params, actual):
         The power to set the power supply to
     """
 
-    calc_error(run_params, actual)
+    calc_error(state, actual)
 
-    _, pid_coeffs, error_history, time_history = run_params
-
+    last_meas_time, last_meas_temp, integral, derivative = state
     p_coeff, i_coeff, d_coeff = pid_coeffs
 
     # Proportional component
-    p_comp = p_coeff * error_history[-1]
+    p_comp = p_coeff * last_meas_temp
 
     # Integral component
-    # Riemann sum over the full 1 second memory
-    time_diffs = []
-    for ind in range(len(time_history) - 1):
-        time_diffs.append(time_history[ind + 1] - time_history[ind])
-    time_diffs.append(time.time() - time_history[-1])
-    integral = numpy.dot(error_history, time_diffs)
     i_comp = i_coeff * integral
 
     # Derivative component
-    # Find the index of the first error recorded more than 0.1 s back
-    current_time = time_history[-1]
-    ind = -2
-    while True:
-        diff = time_history[ind] - current_time
-        if (diff > 0.1) or (ind == -len(time_history)):
-            break
-        else:
-            ind -= 1
-    error_diff = error_history[-1] - error_history[ind]
-    time_diff = time_history[-1] - time_history[ind]
-    derivative = error_diff / time_diff
     d_comp = d_coeff * derivative
 
     return p_comp + i_comp + d_comp
@@ -96,13 +77,6 @@ def main_with_cxn(cxn, target, pid_coeffs):
     error_history = []
     time_history = []
     run_params = [target, pid_coeffs, error_history, time_history]
-
-    # Get a few errors to boot strap the PID loop
-    start_time = time.time()
-    while time.time() - start_time < 1.0:
-        actual = cxn.multimeter_mp730028.measure()
-        calc_error(run_params, actual)
-    power = pid(run_params, actual)
 
     # Start 'Press enter to stop...'
     tool_belt.init_safe_stop()
