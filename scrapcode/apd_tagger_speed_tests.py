f5b4be34424dde3b4acaaa2fdaf14dbe3c798d9e
mccambria <matthew.carl.cambria@gmail.com>
2021-08-31 13:30:40 -0500

    30% improvement

--- scrapcode/apd_tagger_speed_tests.py
+++ scrapcode/apd_tagger_speed_tests.py
@@ -71,9 +71,11 @@ def read_counter_internal(channels):
 
         sample_channels = channels[previous_sample_end_ind:sample_end_ind]
 
-        # Make sure we've got arrays or else comparison won't produce
-        # the boolean array we're looking for when we find gate clicks
-        sample_channels = numpy.array(sample_channels)
+        # Make sure we've got an array for comparison to find click indices
+        # and a list for operations that necessarily scale linearly and
+        # need to be fast.
+        sample_channels_arr = numpy.array(sample_channels)
+        sample_channels_list = sample_channels.tolist()
 
         sample_counts = []
         sample_counts_append = sample_counts.append
@@ -86,13 +88,18 @@ def read_counter_internal(channels):
             gate_close_channel = -gate_open_channel
 
             # Find gate open clicks
-            result = numpy.nonzero(sample_channels == gate_open_channel)
+            # gate_open_click_inds = [
+            #     i
+            #     for i, value in enumerate(sample_channels)
+            #     if value == gate_open_channel
+            # ]
+            result = numpy.nonzero(sample_channels_arr == gate_open_channel)
             gate_open_click_inds = result[0].tolist()
 
             # Find gate close clicks
             # Gate close channel is negative of gate open channel,
             # signifying the falling edge
-            result = numpy.nonzero(sample_channels == gate_close_channel)
+            result = numpy.nonzero(sample_channels_arr == gate_close_channel)
             gate_close_click_inds = result[0].tolist()
 
             # The number of APD clicks is simply the number of items in the
@@ -100,20 +107,27 @@ def read_counter_internal(channels):
             channel_counts = []
             channel_counts_append = channel_counts.append
 
-            for ind in range(len(gate_open_click_inds)):
+            # for ind in range(len(gate_open_click_inds)):
+            #     # pass
+            #     gate_open_click_ind = gate_open_click_inds[ind]
+            #     gate_close_click_ind = gate_close_click_inds[ind]
+            # start = time.time()
+            gate_zip = zip(gate_open_click_inds, gate_close_click_inds)
+            for gate_open_click_ind, gate_close_click_ind in gate_zip:
                 # pass
-                gate_open_click_ind = gate_open_click_inds[ind]
-                gate_close_click_ind = gate_close_click_inds[ind]
-                gate_window = sample_channels[
+                # gate_open_click_ind = gate_open_click_inds[ind]
+                # gate_close_click_ind = gate_close_click_inds[ind]
+
+                gate_window = sample_channels_list[
                     gate_open_click_ind:gate_close_click_ind
                 ]
-                # gate_count = numpy.count_nonzero(gate_window == apd_channel)
-                gate_window = gate_window.tolist()
                 gate_count = gate_window.count(apd_channel)
+
+                # gate_count = gate_close_click_ind - gate_open_click_ind
+
                 channel_counts_append(gate_count)
 
             sample_counts_append(channel_counts)
-
         return_counts_append(sample_counts)
         previous_sample_end_ind = sample_end_ind
 
