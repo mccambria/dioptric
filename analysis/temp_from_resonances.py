0a8e94247ac2669ed238ae9a7da5ca8e6a917f35
mccambria <45080582+mccambria@users.noreply.github.com>
2021-09-14 14:17:18 -0500

    working changes

--- analysis/temp_from_resonances.py
+++ analysis/temp_from_resonances.py
@@ -169,10 +169,12 @@ def main_files(files, mag_B=None, theta_B_deg=None):
 
     resonances = []
     res_errs = []
+    
+    path_from_nvdata = "branch_master/pulsed_resonance/2021_09"
 
     for ind in range(2):
         file = files[ind]
-        data = tool_belt.get_raw_data(file)
+        data = tool_belt.get_raw_data(file, path_from_nvdata)
         res, res_err = return_res_with_error(data)
         resonances.append(res)
         res_errs.append(res_err)
@@ -224,8 +226,8 @@ def main(zfs, zfs_err):
 if __name__ == "__main__":
 
     files = [
-        "2021_09_13-15_29_34-hopper-search",
-        "2021_09_13-15_41_02-hopper-search",
+        "2021_09_14-00_37_51-hopper-search",
+        "2021_09_14-00_46_41-hopper-search",
     ]
 
     main_files(files)
