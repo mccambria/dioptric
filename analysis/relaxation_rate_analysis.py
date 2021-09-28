31ff7b404b2a548a03eaf0ca04158bf5bbb36951
Matt Cambria <matthew.carl.cambria@gmail.com>
2021-09-27 23:32:47 -0500

    working changes

--- analysis/relaxation_rate_analysis.py
+++ analysis/relaxation_rate_analysis.py
@@ -741,7 +741,7 @@ def main(path, folder, omega=None, omega_ste=None, doPlot=False, offset=True):
 
 if __name__ == "__main__":
 
-    temp = 327.5
+    temp = 160
 
     # est_omega = omega_calc(temp)
     # est_gamma = gamma_calc(temp)
