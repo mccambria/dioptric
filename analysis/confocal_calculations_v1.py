d3e7e1634621cbb7fdcbe45083ce6bceb9014787
mccambria <matthew.carl.cambria@gmail.com>
2021-11-18 14:39:08 -0600

    confocal calculations work

--- analysis/confocal_calculations_v1.py
+++ analysis/confocal_calculations_v1.py
@@ -109,6 +109,98 @@ def aperture_propagate(input_field, input_r, output_r, z, aperture_rad):
     return np.array(output_field)
 
 
+def psf_field_integrand(r_prime, r, z, f, k):
+    input_profile = np.exp(-(r_prime ** 2) / (2 * 1e-3 ** 2))
+    # input_profile = 1 / np.sqrt(r_prime ** 2 + f ** 2)
+    # input_profile = 1
+    phase = np.exp(1j * k * (r ** 2 + r_prime ** 2) / (2 * z))
+    bessel = bessel_func(0, -k * r * r_prime / z)
+    return r_prime * input_profile * phase * bessel
+
+
+def psf_field_single(r, z, f, k, norm, phi):
+    psf_field_integrand_lambda = lambda r_prime: psf_field_integrand(
+        r_prime, r, z, f, k
+    )
+    integrand_real = lambda r_prime: np.real(
+        psf_field_integrand_lambda(r_prime)
+    )
+    integrand_imag = lambda r_prime: np.imag(
+        psf_field_integrand_lambda(r_prime)
+    )
+    real = integrate.quad(integrand_real, 0, phi)[0]
+    imag = integrate.quad(integrand_imag, 0, phi)[0]
+    return norm * (real + 1j * imag)
+
+
+def psf_field(r, z, f, k, norm, phi):
+    """Calculate the field associated with the light emitted from an isotropic
+    point emitter located at the focus of a lens
+
+    Parameters
+    ----------
+    r : float
+        Radius in the input plane
+    z : float
+        Distance from the input plane
+    f : float
+        Focal length of the lens
+    k : float
+        Wavenumber of the light
+    norm : float
+        Normalization coefficient
+    phi : float
+        Radius of the lens aperture
+
+    Returns
+    -------
+    np.array(float)
+        1D array of the psf field values along the radial sweep
+    """
+    if type(r) in [list, np.ndarray]:
+        result = []
+        for val in r:
+            result.append(psf_field_single(val, z, f, k, norm, phi))
+        result = np.array(result)
+    else:
+        result = psf_field_single(r, z, f, k, norm, phi)
+    return result
+
+
+def plot_psf():
+
+    num_points = 1000
+
+    z = 100
+    # r_range = z / 1000
+    # r_range = z / 500
+    r_range = 0.05
+    r_linspace = np.linspace(-r_range, +r_range, num_points)
+
+    # r = 0
+    # z_range = 10
+    # z_linspace = np.linspace(-r_range, +r_range, num_points)
+
+    norm = 1 / psf_field_single(
+        0, z, sample_focal_length, wavenumber, 1, sample_aperture_radius
+    )
+
+    field = psf_field(
+        r_linspace,
+        z,
+        # r,  # r_linspace,
+        # z_linspace,  # z,
+        sample_focal_length,
+        wavenumber,
+        norm,
+        sample_aperture_radius,
+    )
+    intensity = np.abs(field) ** 2
+    fig, ax = plt.subplots()
+    ax.plot(r_linspace, intensity)
+    # ax.plot(z_linspace, intensity)
+
+
 def calc_overlap(field_1, field_2, r_linspace):
 
     delta = get_linspace_delta(r_linspace)
