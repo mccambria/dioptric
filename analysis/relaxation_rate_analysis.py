54b48042f23cdf57e5c13e29a9f9a297344ee9d0
mccambria <matthew.carl.cambria@gmail.com>
2022-04-14 09:19:09 -0500

    working changes

--- analysis/relaxation_rate_analysis.py
+++ analysis/relaxation_rate_analysis.py
@@ -776,7 +776,7 @@ def main(path, folder, omega=None, omega_ste=None, doPlot=False, offset=True):
 
 if __name__ == "__main__":
 
-    temp = 400
+    temp = 485
 
     # est_omega = omega_calc(temp)
     # est_gamma = gamma_calc(temp)
