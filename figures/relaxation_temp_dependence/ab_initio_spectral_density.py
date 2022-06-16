7562988e37db3c43474cc13b20b8a79c893f1c88
mccambria <matthew.carl.cambria@gmail.com>
2022-06-16 10:00:22 -0500

    figure work

--- figures/relaxation_temp_dependence/ab_initio_spectral_density.py
+++ figures/relaxation_temp_dependence/ab_initio_spectral_density.py
@@ -101,7 +101,7 @@ def main(
         plot_linspace, smearing_range, freqs, couplings_2
     )
 
-    figsize = [7.0, 5.5]
+    figsize = [6.0, 5.0]
     fig, ax = plt.subplots(figsize=figsize)
 
     # line_width = 2.5
