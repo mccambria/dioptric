1341510b174c300230c8916d9f3ee1f51a908a55
mccambria <45080582+mccambria@users.noreply.github.com>
2022-07-14 19:52:42 -0500

    working changes

--- cfm_control_pane-hahn.py
+++ cfm_control_pane-hahn.py
@@ -235,7 +235,7 @@ def do_four_point_esr(nv_sig, apd_indices, state):
 def do_determine_standard_readout_params(nv_sig, apd_indices):
     
     num_reps = 1e5
-    max_readouts = [20e3]
+    max_readouts = [25e3]
     state = States.LOW
     
     determine_standard_readout_params.main(nv_sig, apd_indices, num_reps, 
