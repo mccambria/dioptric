3b95a0784841bf38a50772c4aa1b64f463ff0060
carterfox <102824456+carterfox@users.noreply.github.com>
2022-09-05 12:11:47 -0500

    wrote resonance in qua

--- servers/timing/sequencelibrary/OPX_sequences/resonance.py
+++ servers/timing/sequencelibrary/OPX_sequences/resonance.py
@@ -1,12 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Sat Sep  3 11:16:25 2022
+Created on Mon Sep  5 11:41:23 2022
 
 @author: carterfox
-
-simple readout sequence for the opx in qua
-
 """
 
 
@@ -16,30 +13,28 @@ from qm.QuantumMachinesManager import QuantumMachinesManager
 from qm.qua import *
 from qm import SimulationConfig
 from configuration import *
+import utils.tool_belt as tool_belt
+from utils.tool_belt import States
+
 
 def qua_program(args, num_reps):
     
-    delay, readout_time, apd_index, laser_name, laser_power = args
-
-    delay = numpy.int64(delay)
-    readout_time = numpy.int64(readout_time)
-
-    period = numpy.int64(delay + readout_time + 300)
-  
+    readout, state, laser_name, laser_power, apd_index = args
     
-    """ from pulse streamer version. just to look at for now
-    train = [(period-200, LOW), (100, HIGH), (100, LOW)]
-    seq.setDigital(pulser_do_daq_clock, train)
+    state = States(state)
+    sig_gen_name = config['Microwaves']['sig_gen_{}'.format(state.name)]
+    uwave_delay = config['Microwaves'][sig_gen_name]['delay']
+    laser_delay = config['Optics'][laser_name]['delay']
+    meas_buffer = config['CommonDurations']['cw_meas_buffer']
+    transient = 0
 
-    train = [(delay, LOW), (readout_time, HIGH), (300, LOW)]
-    seq.setDigital(pulser_do_daq_gate, train)
+    readout = numpy.int64(readout)
+    front_buffer = max(uwave_delay, laser_delay)
+    period = front_buffer + 2 * (transient + readout + meas_buffer)
 
-    train = [(period, HIGH)]
-    #tool_belt.process_laser_seq(pulse_streamer, seq, config, laser_name, laser_power, train)  
-       ### only applies to analog because we won't use feedthrough.
-       ### need to figure out what we need to do with this tool_belt function for analog controls of lasers on the opx
+    # Get what we need out of the wiring dictionary
+    sig_gen_gate_chan_name = 'do_{}_gate'.format(sig_gen_name)
 
-    """
     
     with program() as seq:
         
@@ -49,13 +44,20 @@ def qua_program(args, num_reps):
         save(apd_indices,apd_indices_st)
         counts_gate1_apd_0 = declare(int)  # variable for number of counts
         counts_gate1_apd_1 = declare(int)
+        counts_gate2_apd_0 = declare(int)  # variable for number of counts
+        counts_gate2_apd_1 = declare(int)
         counts_gate1 = [counts_gate1_apd_0,counts_gate1_apd_1]
+        counts_gate2 = [counts_gate2_apd_0,counts_gate2_apd_1]
         
         counts_st = declare_stream()  # stream for counts
         
         times_gate1_apd_0 = declare(int, size=100)  # why input a size??
         times_gate1_apd_1 = declare(int, size=100)
+        times = [times_gate1_apd_0,times_gate1_apd_1]
+        times_gate2_apd_0 = declare(int, size=100)  # why input a size??
+        times_gate2_apd_1 = declare(int, size=100)
         times_gate1 = [times_gate1_apd_0,times_gate1_apd_1]
+        times_gate2 = [times_gate1_apd_0,times_gate1_apd_1]
         
         times_st = declare_stream()
         
@@ -68,19 +70,30 @@ def qua_program(args, num_reps):
             #Either we make this statement turn on the laser with an indefinite pulse, 
             #or we need the sequence to populate a pulse in the configuration for the play statement to grab
             
-            wait(delay) # wait for the delay before starting apds
+            wait(front_buffer + transient) # wait for the delay before starting apds
     
             for apd_ind in apd_indices:   #readout during the readout time on all apds
                 measure("readout", "APD_".format(apd_ind), None, time_tagging.analog(times_gate1[apd_ind], readout_time, counts_gate1[apd_ind]))
-                    
+            
+            wait(front_buffer - uwave_delay + transient + readout + meas_buffer + transient)
+            
+            play("ON", "sig_gen_gate_chan_name", duration=int(readout))  # play microwave pulse. the freq is set from outside the opx. in the opx it should just have the digital switch for the sig gen
+            
+            wait(uwave_delay)
+            
+            for apd_ind in apd_indices:   #readout during the readout time on all apds
+                measure("readout", "APD_".format(apd_ind), None, time_tagging.analog(times_gate2[apd_ind], readout_time, counts_gate2[apd_ind]))
+            
+            wait(meas_buffer+uwave_delay)
+            
             # save the sample to the count stream. sample is a list of gates, which is a list of counts from each apd
             # if there is only one gate, it will be in the same structure as read_counter_simple wants so we are good
            
             # in all sequences, these lists need to be populated with however many gates we have. In this case 2. 
-            counts_apd_0 = [counts_gate1[0]]
-            counts_apd_1 = [counts_gate1[1]]
-            times_apd_0 = [times_gate1[0]]
-            times_apd_1 = [times_gate1[1]]
+            counts_apd_0 = [counts_gate1[0],counts_gate2[0]]
+            counts_apd_1 = [counts_gate1[1],counts_gate2[1]]
+            times_apd_0 = [times_gate1[0],times_gate2[0]]
+            times_apd_1 = [times_gate2[1],times_gate2[1]]
             
             # the code below should apply to all sequences. It takes all the counts from both possible apds and saves it based on which apds are actually in use
             counts_all_apds = [counts_apd_0,counts_apd_1]
