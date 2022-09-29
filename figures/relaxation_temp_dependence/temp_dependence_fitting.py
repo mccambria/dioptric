67e2e5cda32a94d341e73637b33d098d714d5b53
mccambria <matthew.carl.cambria@gmail.com>
2022-09-29 11:50:09 -0500

    last T1 temp dep figure work before submission

--- figures/relaxation_temp_dependence/temp_dependence_fitting.py
+++ figures/relaxation_temp_dependence/temp_dependence_fitting.py
@@ -2573,35 +2573,35 @@ def main_sub(
 
 if __name__ == "__main__":
 
-    temp = 300
-    # delta1 = 4
-    delta1 = 68.2
-    # delta2 = 167
-    # A_1 = 580
-    # A_2 = 9000
-    n1 = bose(delta1, temp)
-    # n2 = bose(delta2, temp)
-    print(n1)
-    # print(A_1 * n1 * (n1 + 1))
-    # print(A_2 * n2 * (n2 + 1))
-    # # print(bose(0.01241, 150))
-    # # print(bose(65, 450) * (bose(65, 450) + 1))
-    # # print(presentation_round_latex(145.88, 26.55))
-    # # print(presentation_round_latex(145.88, 16.55))
-    # # print(presentation_round_latex(145.88, 1.2))
-    # # print(presentation_round_latex(145.88, 6.55))
-    # # print(presentation_round_latex(145.88999, 0.002))
-    # # print(presentation_round_latex(15.88999, 0.00167))
-    # # print(presentation_round_latex(0.0288999, 0.0000167))
-    sys.exit()
+    # temp = 300
+    # # delta1 = 4
+    # delta1 = 68.2
+    # # delta2 = 167
+    # # A_1 = 580
+    # # A_2 = 9000
+    # n1 = bose(delta1, temp)
+    # # n2 = bose(delta2, temp)
+    # print(n1)
+    # # print(A_1 * n1 * (n1 + 1))
+    # # print(A_2 * n2 * (n2 + 1))
+    # # # print(bose(0.01241, 150))
+    # # # print(bose(65, 450) * (bose(65, 450) + 1))
+    # # # print(presentation_round_latex(145.88, 26.55))
+    # # # print(presentation_round_latex(145.88, 16.55))
+    # # # print(presentation_round_latex(145.88, 1.2))
+    # # # print(presentation_round_latex(145.88, 6.55))
+    # # # print(presentation_round_latex(145.88999, 0.002))
+    # # # print(presentation_round_latex(15.88999, 0.00167))
+    # # # print(presentation_round_latex(0.0288999, 0.0000167))
+    # sys.exit()
 
     # tool_belt.init_matplotlib()
     kpl.init_kplotlib()
     matplotlib.rcParams["axes.linewidth"] = 1.0
 
-    plot_type = "rates"
+    # plot_type = "rates"
     # plot_type = "T2_max"
-    # plot_type = "ratios"
+    plot_type = "ratios"
     # plot_type = "ratio_fits"
     # plot_type = 'residuals'
     # plot_type = "normalized_residuals"
