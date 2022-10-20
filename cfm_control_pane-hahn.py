ade4e1b9f1857422b99b40f1123f9ab0e2ce7159
mccambria <45080582+mccambria@users.noreply.github.com>
2022-10-20 14:11:50 -0500

    working changes

--- cfm_control_pane-hahn.py
+++ cfm_control_pane-hahn.py
@@ -319,7 +319,7 @@ def do_pulsed_resonance(nv_sig, apd_indices, freq_center=2.87, freq_range=0.2):
     num_reps = 2e4
     num_runs = 32
     uwave_power = 16.5
-    uwave_pulse_dur = 1000
+    uwave_pulse_dur = 250
 
     pulsed_resonance.main(
         nv_sig,
@@ -1024,8 +1024,8 @@ if __name__ == "__main__":
         # 'coords': [0.0, 0.0, 0], 'name': '{}-search'.format(sample_name),
         # 'coords': [0.205, -0.111, 0], 'name': '{}-search'.format(sample_name),
         # 'disable_opt': True, "disable_z_opt": False, 'expected_count_rate': 15,
-        'coords': [0.106, -0.071, -7], 'name': '{}-nv1_2022_10_18'.format(sample_name),
-        'disable_opt': False, "disable_z_opt": True, 'expected_count_rate': 24,
+        'coords': [0.255, -0.072, 12], 'name': '{}-nv4_2022_10_19'.format(sample_name),
+        'disable_opt': False, "disable_z_opt": True, 'expected_count_rate': 21,
 
         'imaging_laser': green_laser, 'imaging_laser_filter': "nd_0", 'imaging_readout_dur': 1e7,
         # "imaging_laser": green_laser, "imaging_laser_filter": "nd_0.5", "imaging_readout_dur": 1e7,
@@ -1044,7 +1044,7 @@ if __name__ == "__main__":
         # "charge_readout_laser": yellow_laser, "charge_readout_dur": 10e6, "charge_readout_laser_power": 1.0,
 
         'collection_filter': None, 'magnet_angle': None,
-        'resonance_LOW': 2.87, 'rabi_LOW': 500, 'uwave_power_LOW': 16.5,
+        'resonance_LOW': 2.871, 'rabi_LOW': 500, 'uwave_power_LOW': 16.5,
         'resonance_HIGH': 2.882, 'rabi_HIGH': 300, 'uwave_power_HIGH': 16.5,
         }
     # fmt: on
@@ -1076,7 +1076,7 @@ if __name__ == "__main__":
         #                                 )
         
         # for z in np.arange(-15, -100, -5):
-        # for z in np.arange(0, -100, -8):
+        # for z in np.arange(0, 40, 5):
         # # while True:
         #     if tool_belt.safe_stop():
         #         break
@@ -1093,7 +1093,7 @@ if __name__ == "__main__":
         # do_resonance(nv_sig, apd_indices, 2.87, 0.200)
         # do_resonance_state(nv_sig , apd_indices, States.LOW)
         # do_resonance_state(nv_sig, apd_indices, States.HIGH)
-        do_pulsed_resonance(nv_sig, apd_indices, 2.87, 0.040)
+        do_pulsed_resonance(nv_sig, apd_indices, 2.87, 0.020)
         # do_pulsed_resonance_state(nv_sig, apd_indices, States.LOW)
         # do_pulsed_resonance_state(nv_sig, apd_indices, States.HIGH)
         # # # do_scc_resonance(nv_sig, apd_indices, States.LOW)
