e585628a2e64740601bb6f147b5add16b75296fe
mccambria <matthew.carl.cambria@gmail.com>
2022-11-18 20:13:36 -0600

    zfs vs t figure work

--- figures/zfs_vs_t/temp_monitoring.py
+++ figures/zfs_vs_t/temp_monitoring.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-This file contains standardized functions intended to simplify the
-creation of plots for publications in a consistent style.
+Plot temperature montioring data for zfs experiments
 
 Created on November 17th, 2022
 
@@ -11,15 +10,21 @@ Created on November 17th, 2022
 
 # region Imports
 
-
+import utils.common as common
+import utils.kplotlib as kpl
+import utils.tool_belt as tool_belt
+import numpy as np
 import matplotlib.pyplot as plt
-from enum import Enum
-from colorutils import Color
-import re
+import csv
+from datetime import datetime
 
 # endregion
 
 # region Constants
+
+
+nvdata_dir = common.get_nvdata_dir()
+data_file_path = "rsysr"
 
 
 # endregion
@@ -28,42 +33,48 @@ import re
 # region Colors
 
 
-def replot(path_to_file):
-    
+def replot(file_list):
 
-    nvdata_dir = common.get_nvdata_dir()
-    path_to_file = (
-        nvdata_dir
-        # / "pc_hahn/service_logging/old/2022_11_16-calibrated_temp_monitor.log"
-        / "pc_hahn/service_logging/calibrated_temp_monitor.log"
-    )
-    replot(path_to_file)
+    path_to_folder = nvdata_dir / "pc_hahn/service_logging/zfs_vs_t"
 
-    center_time = 1668712861
-    min_time = center_time - (15 * 60)
-    max_time = center_time + (15 * 60)
+    # center_time = 1668712861
+    # min_time = center_time - (15 * 60)
+    # max_time = center_time + (15 * 60)
 
+    # Get all the times and temps out of the log files
     times = []
     temps = []
-
-    with open(path_to_file) as csv_file:
-        reader = csv.reader(csv_file)
-        for row in reader:
-            time = int(row[0])
-            if min_time <= time <= max_time:
-                times.append(time - min_time)
-                temp = float(row[1])
-                temps.append(temp)
+    for log_file in path_to_folder.iterdir():
+        if not log_file.is_file():
+            continue
+        with open(log_file) as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader:
+                times.append(int(row[0]))
+                temps.append(float(row[1]))
 
     kpl.init_kplotlib()
-    fig, ax = plt.subplots()
-    ax.set_xlabel("Time (s)")
-    ax.set_ylabel("Temp (K)")
-    kpl.plot_line(ax, times, temps)
-    kpl.tight_layout(fig)
+
+    for data_file in file_list:
+        # First 19 characters are human-readable timestamp
+        date_time_str = data_file[0:19]
+        date_time = datetime.strptime(date_time_str, r"%Y_%m_%d-%H_%M_%S")
+        date_time.tzinfo = "CST"
+        timestamp = date_time.timestamp()
+        fig, ax = plt.subplots()
+        ax.set_xlabel("Time (s)")
+        ax.set_ylabel("Temp (K)")
+        kpl.plot_line(ax, times, temps)
+        kpl.tight_layout(fig)
+
     plt.show(block=True)
 
 
 # endregion
 
 
+if __name__ == "__main__":
+
+    file_list = ["2022_11_18-11_27_31-wu-nv4_zfs_vs_t"]
+    replot(file_list)
+    # replot(None)
