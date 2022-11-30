67aaf2c8412b2924515bc0b4f6aa8f5554a1cb06
mccambria <matthew.carl.cambria@gmail.com>
2022-11-29 18:11:22 -0600

    archive branches cleanup

--- utils/archive_branches.py
+++ utils/archive_branches.py
@@ -1,13 +1,12 @@
 # -*- coding: utf-8 -*-
-"""This script will archive branches. A branch should be archived if you're
+"""Script for archiving branches. A branch should be archived if you're
 done working with it so that it doesn't clutter the repo. Generally we want
 a record of our work so we don't want to delete branches outright.
 
-Archiving is done by tagging them as archive/<branchname> and removing them
-both locally and remotely. Before each operation, the user is asked for
-confirmation.
+Archiving is done by tagging them as archive/<branchname>-<unix_timestamp> and 
+deleting them both locally and remotely. 
 
-Created on Mon Jun 10 08:02:12 2019
+Created on June 10th, 2019
 
 @author: mccambria
 """
@@ -65,65 +64,66 @@ def main(repo_path, branches_to_archive, skip_merged_check=False):
     print("\nLocal branches:")
     print(local_branches)
 
-    # Confirm that the branch should be deleted
-    archived_branches = []
-    tagged_branches = []
     for branch in branches_to_archive:
-        archive = True
+        do_archive = True
+        tag_created = False
+        tag_pushed = False
         if branch == "master":
             print(
                 "I'm sorry Dave. I'm afraid I can't archive the master branch."
             )
-            archive = False
+            do_archive = False
         elif branch not in local_branches:
             print(
                 f"Branch {branch} does not exist locally for this repo. "
-                "Switch to it first before merging. Skipping..."
+                "Switch to it first before merging."
             )
-            archive = False
+            do_archive = False
         elif (branch not in merged_branches) and not skip_merged_check:
             print(
                 f"Branch {branch} is not fully merged with master. "
-                If you wish to archive anyway, set skip_merged_check to True"
+                "If you wish to archive anyway, set skip_merged_check to True."
             )
-            archive = False
-        if archive:  # Change to if True to override checks
-            # Add a timestamp to the tagged branch
-            inst = int(time.time())
-            tagged_name = "{}-{}".format(branch, inst)
-            repo_git.tag(r"archive/{}".format(tagged_name), branch)
-            archived_branches.append(branch)
-            tagged_branches.append(tagged_name)
+            do_archive = False
 
-    if archived_branches == []:
-        print("No branches archived.")
+        # Move on to the next branch if we can't archive this one
+        if not do_archive:
+            print(f"Skipping branch {branch}.")
+            print()
+            continue
 
-    # Push archive tags to remote
-    for branch in tagged_branches:
+        # Create the tag
+        inst = int(time.time())  # Add a timestamp to the tagged branch
+        tagged_name = f"{branch}-{inst}"
         try:
-            print("tagging")
-            origin.push(r"archive/{}".format(branch))
-            print("tagged")
-        except Exception as e:
-            print(e)
+            repo_git.tag(rf"archive/{tagged_name}", branch)
+            tag_created = True
+        except Exception as exc:
+            print(f"Failed to create tag for branch {branch}")
+            print(exc)
 
-    # Delete remote branches
-    for branch in archived_branches:
-        try:
-            print("deleting remote")
-            origin.push(":{}".format(branch))
-            print("deleted remote")
-        except Exception as e:
-            print(e)
+        # Push the tag
+        if tag_created:
+            try:
+                origin.push(f"archive/{tagged_name}")
+                tag_pushed = True
+            except Exception as exc:
+                print(f"Failed to push tag {tagged_name}")
+                print(exc)
 
-    # Delete local branches
-    for branch in archived_branches:
-        try:
-            print("deleting local")
-            repo_git.branch("-D", branch)
-            print("deleted local")
-        except Exception as e:
-            print(e)
+        # Delete remote and local branches
+        if tag_created and tag_pushed:
+            try:
+                origin.push(f":{branch}")
+            except Exception as exc:
+                print(f"Failed to delete remote branch {branch}")
+                print(exc)
+            try:
+                repo_git.branch("-D", branch)
+            except Exception as exc:
+                print(f"Failed to delete local branch {branch}")
+                print(exc)
+        print()
 
 
 if __name__ == "__main__":
@@ -132,8 +132,36 @@ if __name__ == "__main__":
     repo_path = Path.home() / "Documents/GitHub/kolkowitz-nv-experiment-v1.0"
 
     # List of branches to archive
-    branches_to_archive = ["time-tagger-speedup"]
-    
-    skip_merged_check=True
+    # branches_to_archive = [
+    #     "dioptric",
+    #     "opx_setup",
+    #     "CFMIII",
+    #     "time-tagger-speedup",
+    #     "email-notifications",
+    #     "piezo-hysteresis",
+    #     "Spin_to_charge",
+    #     "hopper_disable_opt",
+    #     "temperature_reading",
+    #     "filter-slider-stops-responding",
+    #     "File_directory_sam",
+    #     "curve-fit-tests",
+    #     "t1_parallel_exp",
+    #     "semi-auto-t1",
+    #     "all-zeroes-investigation",
+    #     "high-to-high",
+    #     "split_resonance",
+    #     "cpmg",
+    #     "ramsey3",
+    #     "analog-aom",
+    #     "resonance_improvements_mcc",
+    #     "working_branch_sam",
+    #     "Samli",
+    # ]
+    branches_to_archive = [
+        "opx-setup",
+        "working_branch_sam",
+    ]
+
+    skip_merged_check = True
 
     main(repo_path, branches_to_archive, skip_merged_check)
