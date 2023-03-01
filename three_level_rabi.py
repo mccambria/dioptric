a312c4684574e1de5b5a3b1e042574c381dedcf6
mccambria <matthew.carl.cambria@gmail.com>
2023-03-01 11:00:55 -0600

    working changes

--- three_level_rabi.py
+++ three_level_rabi.py
@@ -7,9 +7,6 @@ Created on February 28th, 2023
 @author: mccambria
 """
 
-# region Import and constants
-
-
 import numpy as np
 import utils.tool_belt as tool_belt
 import matplotlib.pyplot as plt
@@ -32,11 +29,54 @@ def gen_hamiltonian(dp, Omega, dm):
     )
 
 
+def plot_mat_els():
+
+    Omega = 1
+    # half_splitting = 0.5
+    half_splitting = 10
+    center_freq = 2870
+    wp = center_freq + half_splitting
+    wm = center_freq - half_splitting
+
+    Omegas = np.linspace(-2, 2, 1000)
+
+    x_vals = Omegas
+
+    plot_mags = [[], [], []]
+
+    drive_freq = wp
+    for Omega in Omegas:
+
+        dp = wp - drive_freq
+        dm = wm - drive_freq
+
+        hamiltonian = gen_hamiltonian(dp, Omega, dm)
+        eigvals, eigvecs = np.linalg.eig(hamiltonian)
+        idx = eigvals.argsort()[::-1]
+        eigvals = eigvals[idx]
+        eigvecs = eigvecs[:, idx]
+
+        # plot_mags[0].append(eigvals[0])
+        # plot_mags[1].append(eigvals[1])
+        # plot_mags[2].append(eigvals[2])
+        plot_mags[0].append(np.abs(eigvecs[0][1]) ** 2)
+        plot_mags[1].append(np.abs(eigvecs[1][1]) ** 2)
+        plot_mags[2].append(np.abs(eigvecs[2][1]) ** 2)
+
+    fig, ax = plt.subplots()
+
+    kpl.plot_line(ax, x_vals, plot_mags[0], label="+")
+    kpl.plot_line(ax, x_vals, plot_mags[1], label="0")
+    kpl.plot_line(ax, x_vals, plot_mags[2], label="-")
+
+    ax.legend()
+
+
 def main():
 
     Omega = 1
-    half_splitting = 0.2
-    # half_splitting = 10
+    half_splitting = 1
+    # half_splitting = 5
     center_freq = 2870
     wp = center_freq + half_splitting
     wm = center_freq - half_splitting
@@ -44,14 +84,15 @@ def main():
     drive_freqs = np.linspace(2860, 2880, 1000)
     ts = np.linspace(0, 10 * np.pi / Omega, 1000)
     x_vals = drive_freqs
-    x_vals = ts
+    # x_vals = ts
 
     plot_mags = [[], [], []]
 
     # t = np.pi / Omega
-    # for drive_freq in drive_freqs:
-    drive_freq = center_freq
-    for t in ts:
+    t = 2.8
+    for drive_freq in drive_freqs:
+        # drive_freq = wp
+        # for t in ts:
 
         dp = wp - drive_freq
         dm = wm - drive_freq
@@ -89,5 +130,6 @@ if __name__ == "__main__":
     kpl.init_kplotlib()
 
     main()
+    # plot_mat_els()
 
     plt.show(block=True)
