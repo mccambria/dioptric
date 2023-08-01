5b2b9dc6518105baa46908edfa85252c95e594cf
mccambria <matthew.carl.cambria@gmail.com>
2023-08-01 15:14:10 -0500

    Nuvu camera server

---
+++ servers/inputs/camera_NUVU_hnu512gamma.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Input server for the "HNu 512 Gamma" EMCCD camera from NUVU.
+
+Created on July 31st, 2023
+
+@author: mccambria
+
+### BEGIN NODE INFO
+[info]
+name = camera_NUVU_hnu512gamma
+version = 1.0
+description =
+[startup]
+cmdline = %PYTHON% %FILE%
+timeout = 20
+[shutdown]
+message = 987654321
+timeout = 5
+### END NODE INFO
+"""
+
+
+from labrad.server import LabradServer
+from labrad.server import setting
+from utils import common
+from utils import tool_belt as tb
+import numpy as np
+import socket
+import ctypes
+
+
+class CameraNuvuHnu512gamma(LabradServer):
+    name = "camera_NUVU_hnu512gamma"
+    pc_name = socket.gethostname()
+
+    def initServer(self):
+        tb.configure_logging(self)
+        self.libc = ctypes.CDLL("nc_driver_x64")
+
+    @setting(0)
+    def test(self, c):
+        return self.stream_channels
+
+    @setting(5)
+    def reset(self, c):
+        self.stop_tag_stream_internal()
+
+
+__server__ = CameraNuvuHnu512gamma()
+
+if __name__ == "__main__":
+    from labrad import util
+
+    util.runServer(__server__)
