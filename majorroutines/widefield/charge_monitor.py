ad253e5b1c2688c72b6a241cf02f26fc620c6a01
mccambria <matthew.carl.cambria@gmail.com>
2024-04-13 13:04:32 -0700

    charge state monitoring

--- majorroutines/widefield/charge_monitor.py
+++ majorroutines/widefield/charge_monitor.py
@@ -28,31 +28,77 @@ from utils.constants import LaserKey, NVSig
 
 
 def detect_cosmic_rays(nv_list, num_reps, num_runs, dark_time):
-    charge_prep_fn = base_routine.charge_prep_loop
-    data = main(nv_list, num_reps, num_runs, dark_time, charge_prep_fn)
-    process_detect_cosmic_rays(data)
+    main(
+        nv_list,
+        num_reps,
+        num_runs,
+        dark_time,
+        base_routine.charge_prep_loop,
+        process_detect_cosmic_rays,
+    )
 
 
 def process_detect_cosmic_rays(data):
+    nv_list = data["nv_list"]
+    num_nvs = len(nv_list)
     counts = data["counts"]
     sig_counts = counts[0]
-    states, raw_data = widefield.threshold_counts(nv_list, sig_counts)
+    states, _ = widefield.threshold_counts(nv_list, sig_counts)
+    img_array = np.array([states[nv_ind].flatten() for nv_ind in range(num_nvs)])
+    fig, ax = plt.subplots()
+    kpl.imshow(ax, img_array)
+    return fig
 
 
 def check_readout_fidelity(nv_list, num_reps, num_runs):
     dark_time = 0
-    charge_prep_fn = base_routine.charge_prep_loop_first_rep
-    data = main(nv_list, num_reps, num_runs, dark_time, charge_prep_fn)
-    process_check_readout_fidelity(data)
+    main(
+        nv_list,
+        num_reps,
+        num_runs,
+        dark_time,
+        base_routine.charge_prep_loop_first_rep,
+        process_check_readout_fidelity,
+    )
 
 
 def process_check_readout_fidelity(data):
+    nv_list = data["nv_list"]
+    num_nvs = len(nv_list)
     counts = data["counts"]
+    num_runs = counts.shape[2]
+    num_reps = counts.shape[4]
     sig_counts = counts[0]
-    states, raw_data = widefield.threshold_counts(nv_list, sig_counts)
+    states, _ = widefield.threshold_counts(nv_list, sig_counts)
+
+    fig, ax = plt.subplots(2, 1)
+    labels = {0: "NV0", 1: "NV-"}
+    for init_state in [0, 1]:
+        for nv_ind in range(num_nvs):
+            num_shots = 0
+            num_same_shots = 0
+            for run_ind in range(num_runs):
+                for rep_ind in range(num_reps):
+                    prev_state = (
+                        1 if rep_ind == 0 else states[nv_ind, run_ind, 0, rep_ind - 1]
+                    )
+                    current_state = states[nv_ind, run_ind, 0, rep_ind]
+                    if prev_state == init_state:
+                        num_shots += 1
+                        if current_state == prev_state:
+                            num_same_shots += 1
+            prob = num_same_shots / num_shots
+            err = num_shots * prob * (1 - prob)
+            kpl.plot_points(ax, nv_ind, num_same_shots / num_shots, yerr=err)
+
+        ax.set_xlabel("NV index")
+        label = labels[init_state]
+        ax.set_xlabel(f"P({label}|previous shot {label})")
+
+    return fig
 
 
-def main(nv_list, num_reps, num_runs, dark_time, charge_prep_fn):
+def main(nv_list, num_reps, num_runs, dark_time, charge_prep_fn, data_processing_fn):
     ### Some initial setup
     seq_file = "charge_monitor.py"
 
@@ -66,8 +112,7 @@ def main(nv_list, num_reps, num_runs, dark_time, charge_prep_fn):
 
     def run_fn(shuffled_step_inds):
         pol_coords_list = widefield.get_coords_list(nv_list, LaserKey.CHARGE_POL)
-        ion_coords_list = widefield.get_coords_list(nv_list, LaserKey.ION)
-        seq_args = [pol_coords_list, ion_coords_list, dark_time]
+        seq_args = [pol_coords_list, dark_time]
         seq_args_string = tb.encode_seq_args(seq_args)
         pulse_gen.stream_load(seq_file, seq_args_string, num_reps)
 
@@ -81,20 +126,25 @@ def main(nv_list, num_reps, num_runs, dark_time, charge_prep_fn):
         charge_prep_fn=charge_prep_fn,
     )
 
-    ### Save and clean up
-
     repr_nv_sig = widefield.get_repr_nv_sig(nv_list)
     repr_nv_name = repr_nv_sig.name
     timestamp = dm.get_time_stamp()
-    file_path = dm.get_file_path(__file__, timestamp, repr_nv_name)
     raw_data |= {
         "timestamp": timestamp,
     }
+
+    try:
+        fig = data_processing_fn(raw_data)
+    except Exception:
+        fig = None
+
+    ### Save and clean up
+
+    file_path = dm.get_file_path(__file__, timestamp, repr_nv_name)
     dm.save_raw_data(raw_data, file_path)
-
+    if fig is not None:
+        dm.save_figure(fig, file_path)
     tb.reset_cfm()
-
-    return data
 
 
 if __name__ == "__main__":
@@ -105,9 +155,6 @@ if __name__ == "__main__":
 
     nv_list = data["nv_list"]
     nv_list = [NVSig(**nv) for nv in nv_list]
-    num_nvs = len(nv_list)
-    sig_counts_lists = data["sig_counts_lists"]
-    ref_counts_lists = data["ref_counts_lists"]
-    num_shots = len(sig_counts_lists[0])
+    data["nv_list"] = nv_list
 
     kpl.show(block=True)
