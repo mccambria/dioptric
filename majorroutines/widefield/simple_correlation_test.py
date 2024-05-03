52b50321162d4cdb1f8d857c68f9e65979ae0c70
mccambria <matthew.carl.cambria@gmail.com>
2024-05-02 22:25:52 -0700

    working changes

--- majorroutines/widefield/simple_correlation_test.py
+++ majorroutines/widefield/simple_correlation_test.py
@@ -25,7 +25,8 @@ def process_and_plot(data):
     counts = np.array(data["counts"])
     # counts = data["states"]
     num_nvs = len(nv_list)
-    exclude_inds = (6, 9, 13)
+    # exclude_inds = (6, 9, 13)
+    exclude_inds = ()
     nv_list = [nv_list[ind] for ind in range(num_nvs) if ind not in exclude_inds]
     num_nvs = len(nv_list)
 
@@ -59,7 +60,7 @@ def process_and_plot(data):
     figs = []
     titles = ["Signal", "Reference", "Ideal signal"]
     vals = [sig_corr_coeffs, ref_corr_coeffs, ideal_sig_corr_coeffs]
-    cbar_maxes = [sig_max, ref_max, 1]
+    cbar_maxes = [sig_max, sig_max, 1]
     for ind in range(len(vals)):
         fig, ax = plt.subplots()
         cbar_max = cbar_maxes[ind]
@@ -144,7 +145,7 @@ def main(nv_list, num_reps, num_runs):
 if __name__ == "__main__":
     kpl.init_kplotlib()
 
-    data = dm.get_raw_data(file_id=1519183146997)
+    data = dm.get_raw_data(file_id=1519615059736)
 
     nv_list = data["nv_list"]
     counts = np.array(data["counts"])
