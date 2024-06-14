00bc0d874d66a634c1b3422e7563a25e6dae32a1
mccambria <matthew.carl.cambria@gmail.com>
2024-06-13 17:28:38 -0700

    working changes

--- config/purcell.py
+++ config/purcell.py
@@ -695,7 +695,7 @@ opx_config = {
         # "red_aod_cw-opti": {"type": "constant", "sample": 0.10},
         "red_aod_cw-opti": {"type": "constant", "sample": 0.13},
         "red_aod_cw-ion": {"type": "constant", "sample": 0.13},
-        "red_aod_cw-scc": {"type": "constant", "sample": 0.13},
+        "red_aod_cw-scc": {"type": "constant", "sample": 0.125},
         # Yellow AOM
         "yellow_imaging": {"type": "constant", "sample": 0.40},  # 0.35
         # "yellow_imaging": {"type": "constant", "sample": 0.50},  # 0.35
