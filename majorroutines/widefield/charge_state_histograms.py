df198f4b83bbaa598e8bbc5b5c943eb342bf923c
mccambria <matthew.carl.cambria@gmail.com>
2024-11-15 20:37:41 -0800

    robust fitting work

--- majorroutines/widefield/charge_state_histograms.py
+++ majorroutines/widefield/charge_state_histograms.py
@@ -344,7 +344,7 @@ def main(
 
 if __name__ == "__main__":
     kpl.init_kplotlib()
-    data = dm.get_raw_data(file_id=1688554695897, load_npz=False)
+    data = dm.get_raw_data(file_id=1701595753308, load_npz=False)
     # data = dm.get_raw_data(file_id=1691569540529, load_npz=False)
-    process_and_plot(data, do_plot_histograms=False)
+    process_and_plot(data, do_plot_histograms=True)
     kpl.show(block=True)
