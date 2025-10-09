f3a74e886c952c133d5c84a118f9bdd55d6410c3
sarojc23 <sarojc23@gmail.com>
2025-10-08 18:01:15 -0700

    scc snr check

--- analysis/sc_scc_snr_check_analysis.py
+++ analysis/sc_scc_snr_check_analysis.py
@@ -442,7 +442,7 @@ if __name__ == "__main__":
     # data = dm.get_raw_data(file_stem=file_stem)
 
     data = dm.get_raw_data(
-        file_stem="2025_10_08-17_21_50-rubin-nv0_2025_09_08", load_npz=True
+        file_stem="2025_10_08-15_06_22-rubin-nv0_2025_09_08", load_npz=True
     )
     # file_name = dm.get_file_name(file_id=file_id)
     # print(f"{file_name}_{file_id}")
